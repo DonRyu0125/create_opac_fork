b6d26b68c48575f3baa1a4004bf3ccdfef41844b
mikah13 <anhminhhoang13@gmail.com>
2024-01-09 08:22:14 -0800

    feat: change file name to static

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        inlineDynamicImports: true,
+        entryFileNames: '[name].js', // currently does not work for the legacy bundle
+        assetFileNames: '[name].[ext]', // currently does not work for images
+      },
+    },
+  },
 });
