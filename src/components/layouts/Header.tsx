58d411f1c758c3610f0d759680f95d4a82c2efd8
mikah13 <anhminhhoang13@gmail.com>
2024-01-09 19:00:57 -0800

    feat: add navigations bar to header

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,7 +1,7 @@
 import { useConfig } from '@/hooks/useConfig';
 
 const Header = () => {
-  const { logo } = useConfig();
+  const { logo, navigations } = useConfig();
   return (
     <header className='bg-white'>
       <div className='mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8'>
@@ -16,65 +16,16 @@ const Header = () => {
           <div className='md:flex md:items-center md:gap-12'>
             <nav aria-label='Global' className='hidden md:block'>
               <ul className='flex items-center gap-6 text-sm'>
-                <li>
-                  <a
-                    className='text-gray-500 transition hover:text-gray-500/75'
-                    href='/'
-                  >
-                    {' '}
-                    About{' '}
-                  </a>
-                </li>
-
-                <li>
-                  <a
-                    className='text-gray-500 transition hover:text-gray-500/75'
-                    href='/'
-                  >
-                    {' '}
-                    Careers{' '}
-                  </a>
-                </li>
-
-                <li>
-                  <a
-                    className='text-gray-500 transition hover:text-gray-500/75'
-                    href='/'
-                  >
-                    {' '}
-                    History{' '}
-                  </a>
-                </li>
-
-                <li>
-                  <a
-                    className='text-gray-500 transition hover:text-gray-500/75'
-                    href='/'
-                  >
-                    {' '}
-                    Services{' '}
-                  </a>
-                </li>
-
-                <li>
-                  <a
-                    className='text-gray-500 transition hover:text-gray-500/75'
-                    href='/'
-                  >
-                    {' '}
-                    Projects{' '}
-                  </a>
-                </li>
-
-                <li>
-                  <a
-                    className='text-gray-500 transition hover:text-gray-500/75'
-                    href='/'
-                  >
-                    {' '}
-                    Blog{' '}
-                  </a>
-                </li>
+                {navigations.map((nav) => (
+                  <li key={nav.title}>
+                    <a
+                      className='text-gray-500 transition hover:text-gray-500/75'
+                      href={nav.url}
+                    >
+                      {nav.title}
+                    </a>
+                  </li>
+                ))}
               </ul>
             </nav>
 
