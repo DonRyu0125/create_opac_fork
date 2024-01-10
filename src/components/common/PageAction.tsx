db365129480a818b135280d1397aeff00a44176d
mikah13 <anhminhhoang13@gmail.com>
2024-01-10 10:03:29 -0800

    feat: added page view toggler

--- src/components/common/PageAction.tsx
+++ src/components/common/PageAction.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import { LayoutGrid, List } from 'lucide-react';
 import { Button } from '../ui/button';
 import { Separator } from '../ui/separator';
 import Breadcrumb from './Breadcrumb';
+import { useAtom } from 'jotai';
+import { viewAtom } from '../store';
 
 type Props = {};
 
 const PageAction = (props: Props) => {
+  const [view, setView] = useAtom(viewAtom);
+  const toggleView = () => {
+    setView(view === 'grid' ? 'list' : 'grid');
+  };
   return (
     <div>
       <div className='container flex flex-col items-start justify-between space-y-2 py-4 sm:flex-row sm:items-center sm:space-y-0 md:h-16'>
@@ -21,9 +27,20 @@ const PageAction = (props: Props) => {
         />
 
         <div>
-          <Button>12</Button>
-          <Button>12</Button>
-          <Button>12</Button>
+          <Button
+            onClick={toggleView}
+            variant={view === 'grid' ? 'default' : 'outline'}
+            size='icon'
+          >
+            <LayoutGrid className='h-4 w-4' />
+          </Button>
+          <Button
+            onClick={toggleView}
+            variant={view === 'list' ? 'default' : 'outline'}
+            size='icon'
+          >
+            <List className='h-4 w-4' />
+          </Button>
         </div>
       </div>
 
