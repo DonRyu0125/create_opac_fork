a1982f0e062aca9c5d114ff5e9eceda5d3e0addb
mikah13 <anhminhhoang13@gmail.com>
2024-01-09 18:43:16 -0800

    feat: added header

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import './App.css';
 import Hero from './components/common/Hero';
 import SearchForm from './components/common/SearchForm';
+import Layout from './components/layouts';
 
 function App() {
   return (
-    <>
+    <Layout>
       <Hero title={'Search'} description={'Lorem ipsum'} backgroundImage={''}>
         <SearchForm searchURL={'/action'} inputName={'KEYWORD_CL'} />
       </Hero>
-    </>
+    </Layout>
   );
 }
 
