86344e7daee5544ba061fc43b9634594dfb82118
mikah13 <anhminhhoang13@gmail.com>
2024-01-13 09:03:40 -0800

    Feat: added ThemeToggler

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import ErrorBoundary from './providers/ErrorBoundary.tsx';
+import { ThemeProvider } from './providers/Theme.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ErrorBoundary>
-      <App />
+      <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
+        <App />
+      </ThemeProvider>
     </ErrorBoundary>
   </React.StrictMode>
 );
