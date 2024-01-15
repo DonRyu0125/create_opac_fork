fd90689908e46296a7e3329ba1d48b5db2a92051
mikah13 <anhminhhoang13@gmail.com>
2024-01-15 13:48:21 -0800

    added border-primary to components

--- src/components/common/CollapseList.tsx
+++ src/components/common/CollapseList.tsx
@@ -2,10 +2,10 @@ import {
   Collapsible,
   CollapsibleContent,
   CollapsibleTrigger,
-} from "@/components/ui/collapsible";
-import { ChevronDown, ChevronUp } from "lucide-react";
+} from '@/components/ui/collapsible';
+import { ChevronDown, ChevronUp } from 'lucide-react';
 
-import React from "react";
+import React from 'react';
 
 export interface CollapseListProps
   extends React.HTMLAttributes<HTMLDivElement> {
@@ -20,19 +20,19 @@ const CollapseList = ({
 }: CollapseListProps) => {
   const [isOpen, setIsOpen] = React.useState(expand);
   return (
-    <div className="w-full space-y-2">
+    <div className='w-full space-y-2'>
       <Collapsible
         open={isOpen}
         onOpenChange={setIsOpen}
-        className="overflow-hidden rounded-md border border-input  [&_summary::-webkit-details-marker]:hidden"
+        className='overflow-hidden rounded-md border border-primary  [&_summary::-webkit-details-marker]:hidden'
       >
-        <CollapsibleTrigger className="flex cursor-pointer items-center justify-between py-2 px-2.5 text-gray-900 transition w-full">
-          <span className="text-sm "> {title} </span>
+        <CollapsibleTrigger className='flex cursor-pointer items-center justify-between py-2 px-2.5 text-gray-900 transition w-full'>
+          <span className='text-sm '> {title} </span>
           <span>
             {isOpen ? (
-              <ChevronUp className="w-4 h-4 opacity-50" />
+              <ChevronUp className='w-4 h-4 opacity-50' />
             ) : (
-              <ChevronDown className="w-4 h-4 opacity-50" />
+              <ChevronDown className='w-4 h-4 opacity-50' />
             )}
           </span>
         </CollapsibleTrigger>
