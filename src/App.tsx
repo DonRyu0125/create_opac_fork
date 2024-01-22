423e56d0763589c184b01743e36d0239094aa2ca
mikah13 <anhminhhoang13@gmail.com>
2024-01-21 19:38:14 -0800

    update: remove unused vars

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import Home from '@/components/page-template/Home';
-import Summary from './components/page-template/Summary';
-import Detail from './components/page-template/Detail';
 import { ROUTES } from './router';
 import NotFoundPage from './components/page-template/NotFoundPage';
 
