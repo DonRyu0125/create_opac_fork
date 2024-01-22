9717fc67ca276cacc8b834798b05747197906beb
mikah13 <anhminhhoang13@gmail.com>
2024-01-22 13:46:51 -0800

    update: gen theme script, added Map example

--- src/components/common/Map.tsx
+++ src/components/common/Map.tsx
@@ -1,9 +1,23 @@
-import React from "react";
-
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 type Props = {};
 
 const Map = (props: Props) => {
-  return <div className="w-1/2 h-1/2"></div>;
+  return (
+    <div className='w-1/2 h-1/2'>
+      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
 };
 
 export default Map;
