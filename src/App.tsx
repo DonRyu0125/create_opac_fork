5724c84bdb5a6928f5cf391f6d9ae7d5ec0407ce
mikah13 <anhminhhoang13@gmail.com>
2024-01-21 19:29:11 -0800

    update: use Router in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import Home from "@/components/page-template/Home";
-import Summary from "./components/page-template/Summary";
-import Detail from "./components/page-template/Detail";
+import Home from '@/components/page-template/Home';
+import Summary from './components/page-template/Summary';
+import Detail from './components/page-template/Detail';
+import { ROUTES } from './router';
 
 function App() {
-  return <Home />;
+  const curPage = 'home';
+  return ROUTES[curPage];
 }
 
 export default App;
