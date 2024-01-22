461c2126b683e4348409d166d1f47ab145074bad
mikah13 <anhminhhoang13@gmail.com>
2024-01-21 19:20:54 -0800

    update: remove unused map

--- src/components/common/Map.tsx
+++ src/components/common/Map.tsx
@@ -1,22 +1,9 @@
 import React from 'react';
-import MapGL from 'react-map-gl';
 
 type Props = {};
 
 const Map = (props: Props) => {
-  return (
-    <div className='w-1/2 h-1/2'>
-      <MapGL
-        initialViewState={{
-          longitude: -122.4,
-          latitude: 37.8,
-          zoom: 14,
-        }}
-        style={{ width: 600, height: 400 }}
-        mapStyle='mapbox://styles/mapbox/streets-v9'
-      />
-    </div>
-  );
+  return <div className='w-1/2 h-1/2'></div>;
 };
 
 export default Map;
