2afb7c7fae04c4158f7ba0fb576c504108de52c9
mikah13 <anhminhhoang13@gmail.com>
2024-01-21 19:21:17 -0800

    update: format code

--- src/components/common/Map.tsx
+++ src/components/common/Map.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 
 type Props = {};
 
 const Map = (props: Props) => {
-  return <div className='w-1/2 h-1/2'></div>;
+  return <div className="w-1/2 h-1/2"></div>;
 };
 
 export default Map;
