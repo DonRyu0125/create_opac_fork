f02ae44670ab730c80b6a14417082ddad85eb50d
mikah13 <anhminhhoang13@gmail.com>
2024-01-21 19:36:20 -0800

    added default route to not found page

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import Home from '@/components/page-template/Home';
 import Summary from './components/page-template/Summary';
 import Detail from './components/page-template/Detail';
 import { ROUTES } from './router';
+import NotFoundPage from './components/page-template/NotFoundPage';
 
 function App() {
-  const curPage = 'home';
-  return ROUTES[curPage];
+  const curPage = 'test';
+  const Component = ROUTES[curPage] || NotFoundPage;
+  return <Component />;
 }
 
 export default App;
