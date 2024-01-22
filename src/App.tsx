b806b6c6e7d02cf2f7cdcea2d39e37efd2854971
mikah13 <anhminhhoang13@gmail.com>
2024-01-22 10:07:19 -0800

    update: update script to run theme/schema on start, added comments on routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import { ROUTES } from './router';
 import NotFoundPage from './components/page-template/NotFoundPage';
 
 function App() {
-  const curPage = 'test';
+  const curPage = 'home'; // look at ROUTES object to find matching key /
+  // component to be render.E.G if curPage === home then render Home component and so one
   const Component = ROUTES[curPage] || NotFoundPage;
   return <Component />;
 }
