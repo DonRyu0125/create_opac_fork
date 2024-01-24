8b1b541f3d1d982f2c4ccea7dc0828d2db2a3bb7
mikah13 <anhminhhoang13@gmail.com>
2024-01-24 14:48:09 -0800

    update: added json content for FAQ

--- src/components/page-template/FAQ.tsx
+++ src/components/page-template/FAQ.tsx
@@ -1,11 +1,20 @@
+import { faq } from '@/constants';
 import AccordionList from '../common/AccordionList';
+import Hero from '../common/Hero';
 import Section from '../common/Section';
 import SplitSection from '../common/SplitSection';
 import Layout from '../layouts';
 
 const FAQ = () => {
+  const { heading, subHeading, heroBanner } = faq;
   return (
     <Layout>
+      <Hero
+        className=''
+        title={heading}
+        description={subHeading}
+        backgroundImage={heroBanner}
+      ></Hero>
       <Section heading='FAQ'>
         <AccordionList />
       </Section>
