93d81757e55a9d5bdd85d45a4f92a76e2bc6867a
mikah13 <anhminhhoang13@gmail.com>
2024-01-24 09:43:18 -0800

    style: readjust brightness on hovercard

--- src/components/common/HoverCard.tsx
+++ src/components/common/HoverCard.tsx
@@ -27,17 +27,13 @@ const HoverCard = ({
     >
       <div className="relative mb-4 before:content-[''] before:rounded-md before:absolute before:inset-0 before:bg-black before:bg-opacity-20">
         <img
-          className='w-full brightness-75 group-hover:opacity-90'
+          className='w-full brightness-[65%] group-hover:opacity-90'
           alt={title}
           src={thumbnail}
         />
-        <div className='test__body absolute inset-0 p-8 text-white flex flex-col'>
+        <div className='absolute inset-0 p-8 text-white flex flex-col'>
           <div className='relative'>
-            <a
-              className='test__link absolute inset-0'
-              target='_blank'
-              href='/'
-            ></a>
+            <a className=' absolute inset-0' target='_blank' href='/'></a>
           </div>
           <div className='absolute inset-0 flex flex-col items-start justify-end p-6'>
             <span className='mt-3 inline-block bg-primary px-3 py-1 text-md  font-medium  tracking-wide max-w-full break-words  text-white'>
