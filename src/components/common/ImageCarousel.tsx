9efe4e75f31f51fee4b5a9030cca057d0e67c8d4
mikah13 <anhminhhoang13@gmail.com>
2024-01-24 12:50:56 -0800

    update image carousel

--- src/components/common/ImageCarousel.tsx
+++ src/components/common/ImageCarousel.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react';
 
-import Slide from "./Slide";
-import { ArrowLeftIcon, ChevronLeft, ChevronRight, Scan } from "lucide-react";
+import Slide from './Slide';
+import { ArrowLeftIcon, ChevronLeft, ChevronRight, Scan } from 'lucide-react';
 
 export type ImageProps = {
   src: string;
@@ -17,31 +17,28 @@ const ImageCarousel = ({ items, renderItems }: ImageCarouselProps) => {
   const [current, setCurrent] = React.useState(0);
   const currentImage = items[current];
   return (
-    <div className="flex flex-col space-y-4">
+    <div className='flex flex-col space-y-4'>
       <Slide
-        carouselStyle="sm:basis-1/4"
+        carouselStyle='sm:basis-1/4'
         items={items}
         renderItem={(item, index) => (
-          <div className="cursor-pointer" onClick={() => setCurrent(index)}>
+          <div className='cursor-pointer' onClick={() => setCurrent(index)}>
             {renderItems(item, index)}
           </div>
         )}
         auto={false}
       />
 
-      <div className="flex w-full group cursor-pointer relative">
-        <img className="mx-auto" {...currentImage} />
+      <div className='flex w-full group cursor-pointer relative'>
+        <img className='mx-auto' {...currentImage} />
         <Scan
-          strokeWidth={"3px"}
-          className="absolute bg-primary-foreground/20 w-8 h-8 text-white hover:text-primary bottom-2 right-2 transition-all ease-in duration-400 "
+          strokeWidth={'3px'}
+          className='absolute bg-primary-foreground/20 w-8 h-8 text-white hover:text-primary bottom-2 right-2 transition-all ease-in duration-400 '
         />
 
-        <ChevronLeft
-          className="absolute bg-primary-foreground/10 w-8 h-8 text-white hover:text-primary left-2 top-1/2 transition-all ease-in duration-400"
-          onClick={}
-        />
+        {/* <ChevronLeft className='absolute bg-primary-foreground/20 w-8 h-8 text-red-500 hover:text-primary left-2 top-1/2  transition-all ease-in duration-400 ' />
 
-        <ChevronRight className="absolute bg-primary-foreground/10 w-8 h-8 text-white hover:text-primary right-2 top-1/2 transition-all ease-in duration-400 " />
+        <ChevronRight className='absolute bg-primary-foreground/20 w-8 h-8 text-white hover:text-primary right-2 top-1/2 transition-all ease-in duration-400 ' /> */}
       </div>
     </div>
   );
