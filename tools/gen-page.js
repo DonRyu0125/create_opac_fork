d5c5eccf5606538bdf02225651793b4e2e148b78
mikah13 <anhminhhoang13@gmail.com>
2024-01-24 21:54:03 -0800

    feat: added dependencies for gen-page

--- tools/gen-page.js
+++ tools/gen-page.js
@@ -3,7 +3,8 @@ import chalk from 'chalk';
 
 function main() {
     console.log(chalk.bgBlueBright('Hello world!'));
-
+    // eslint-disable-next-line no-undef
+    console.log(process.argv[2])
 }
 
 main();
