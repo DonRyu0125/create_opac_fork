170e611c2bf659022404b6046f053c0a796cb24a
mikah13 <anhminhhoang13@gmail.com>
2024-01-25 21:07:02 -0800

    update:remove sentry

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,8 +1,6 @@
 /// <reference types="vite/client" />
 
-interface ImportMetaEnv {
-  readonly SENTRY_API_KEY: string;
-}
+interface ImportMetaEnv {}
 
 interface ImportMeta {
   readonly env: ImportMetaEnv;
