2cd81fd1e1bc6bd95bc03c63dbb69580a7222e8b
mikah13 <anhminhhoang13@gmail.com>
2024-02-02 02:27:57 -0800

    update ford logo and search form style

--- src/components/common/SearchForm.tsx
+++ src/components/common/SearchForm.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import { Input } from "@/components/ui/input";
-import { Button } from "../ui/button";
-import { cn } from "@/lib/utils";
-import { Search } from "lucide-react";
+import React from 'react';
+import { Input } from '@/components/ui/input';
+import { Button } from '../ui/button';
+import { cn } from '@/lib/utils';
+import { Search } from 'lucide-react';
 
 export interface SearchFormProps extends React.HTMLAttributes<HTMLFormElement> {
   searchURL: string;
@@ -16,25 +16,29 @@ const SearchForm = ({
 }: SearchFormProps) => {
   return (
     <form
-      method="POST"
+      method='POST'
       action={searchURL}
-      className={cn("w-full mx-auto", className)}
+      className={cn('w-full mx-auto flex space-x-4 justify-center', className)}
       {...props}
     >
-      <div className="relative">
+      <div className='w-96'>
         <Input
           name={inputName}
-          className="w-full  py-3 rounded-lg "
-          placeholder="Search for content..."
-          type="search"
+          className='w-full rounded-none  py-3 bg-white  text-white'
+          placeholder='What are you looking for'
+          type='search'
         />
-        <Button className="absolute right-0 top-0 h-full px-4" type="submit">
-          <span className="hidden md:block"> Search</span>
-          <span className="md:hidden block">
-            <Search className="w-4 h-4" />
-          </span>
-        </Button>
       </div>
+      <Button
+        variant={'secondary'}
+        className='right-0 top-0 h-full px-4'
+        type='submit'
+      >
+        <span className='hidden md:block'> Search</span>
+        <span className='md:hidden block'>
+          <Search className='w-4 h-4' />
+        </span>
+      </Button>
     </form>
   );
 };
--- src/components/page-template/Home.tsx
+++ src/components/page-template/Home.tsx
@@ -1,59 +1,47 @@
-import { home } from "@/constants";
-import { truncateWords } from "@/lib/utils";
-import Hero from "../common/Hero";
-import HoverCard from "../common/HoverCard";
-import Masonry from "../common/Masonry";
-import SearchForm from "../common/SearchForm";
-import Section from "../common/Section";
-import Slide from "../common/Slide";
-import ThumbnailCard from "../common/ThumbnailCard";
-import Layout from "../layouts";
-import { Card } from "../ui/card";
+import { home } from '@/constants';
+import { truncateWords } from '@/lib/utils';
+import Hero from '../common/Hero';
+import HoverCard from '../common/HoverCard';
+import Masonry from '../common/Masonry';
+import SearchForm from '../common/SearchForm';
+import Section from '../common/Section';
+import Slide from '../common/Slide';
+import ThumbnailCard from '../common/ThumbnailCard';
+import Layout from '../layouts';
+import { Card } from '../ui/card';
 const pics = [
-  "https://picsum.photos/1000/800/?random=123",
-  "https://picsum.photos/500/600/?random=456",
-  "https://picsum.photos/900/700/?random=789",
-  "https://picsum.photos/600/400/?random=321",
-  "https://picsum.photos/1200/900/?random=654",
-  "https://picsum.photos/800/500/?random=987",
-  "https://picsum.photos/1000/700/?random=123",
-  "https://picsum.photos/700/800/?random=234",
-  "https://picsum.photos/450/600/?random=567",
-  "https://picsum.photos/800/600/?random=890",
-  "https://picsum.photos/550/450/?random=123",
-  "https://picsum.photos/1000/600/?random=456",
-  "https://picsum.photos/400/300/?random=789",
-  "https://picsum.photos/900/800/?random=321",
-  "https://picsum.photos/1200/900/?random=654",
-  "https://picsum.photos/700/400/?random=987",
-  "https://picsum.photos/800/700/?random=123",
-  "https://picsum.photos/600/500/?random=234",
-  "https://picsum.photos/800/600/?random=567",
-  "https://picsum.photos/500/800/?random=890",
+  'https://picsum.photos/1000/800/?random=123',
+  'https://picsum.photos/500/600/?random=456',
+  'https://picsum.photos/900/700/?random=789',
+  'https://picsum.photos/600/400/?random=321',
+  'https://picsum.photos/1200/900/?random=654',
+  'https://picsum.photos/800/500/?random=987',
+  'https://picsum.photos/1000/700/?random=123',
+  'https://picsum.photos/700/800/?random=234',
+  'https://picsum.photos/450/600/?random=567',
+  'https://picsum.photos/800/600/?random=890',
+  'https://picsum.photos/550/450/?random=123',
+  'https://picsum.photos/1000/600/?random=456',
+  'https://picsum.photos/400/300/?random=789',
+  'https://picsum.photos/900/800/?random=321',
+  'https://picsum.photos/1200/900/?random=654',
+  'https://picsum.photos/700/400/?random=987',
+  'https://picsum.photos/800/700/?random=123',
+  'https://picsum.photos/600/500/?random=234',
+  'https://picsum.photos/800/600/?random=567',
+  'https://picsum.photos/500/800/?random=890',
 ];
 
 const Home = () => {
-  const {
-    heading,
-    subHeading,
-    heroBanner,
-    browseByCategoryTitle,
-    categoriesItems,
-  } = home;
-
+  const { heading, heroBanner, browseByCategoryTitle, categoriesItems } = home;
 
   return (
     <Layout>
-      <Hero
-        className=""
-        title={heading}
-        description={subHeading}
-        backgroundImage={heroBanner}
-      >
+      <Hero className='' title={heading} backgroundImage={heroBanner}>
         <SearchForm
-          className="w-full mt-6 max-w-2xl"
-          searchURL={"/action"}
-          inputName={"KEYWORD_CL"}
+          className='w-full mt-6 max-w-2xl'
+          searchURL={'/action'}
+          inputName={'KEYWORD_CL'}
         />
       </Hero>
       <Section heading={browseByCategoryTitle}>
@@ -63,7 +51,7 @@ const Home = () => {
           items={categoriesItems}
           renderItem={(item, index) => (
             <Card
-              className="max-w-md mx-auto shadow-xl border-none cursor-pointer"
+              className='max-w-md mx-auto shadow-xl border-none cursor-pointer'
               key={index}
             >
               <ThumbnailCard
@@ -82,16 +70,16 @@ const Home = () => {
       >
         <Map />
       </Section> */}
-      <Section heading={"Recent Addition"}>
+      <Section heading={'Recent Addition'}>
         <Masonry
           items={pics}
           renderItem={(item, index) => (
             <HoverCard
-              description="More than 100,000 archival photos, maps, documents, and oral histories, as well as over 5,000 artifacts are at your fingertips. Browse the categories, neighbourhoods, "
+              description='More than 100,000 archival photos, maps, documents, and oral histories, as well as over 5,000 artifacts are at your fingertips. Browse the categories, neighbourhoods, '
               key={index}
-              title={truncateWords("Test", 10)}
+              title={truncateWords('Test', 10)}
               thumbnail={item}
-              url={""}
+              url={''}
             />
           )}
         />
