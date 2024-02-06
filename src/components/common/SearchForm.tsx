b06e35b2d93d0bb17a45b6366899ee1c5531a08c
mikah13 <anhminhhoang13@gmail.com>
2024-02-05 22:01:18 -0800

    restyle search form

--- src/components/common/SearchForm.tsx
+++ src/components/common/SearchForm.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Input } from '@/components/ui/input';
 import { Button } from '../ui/button';
 import { cn } from '@/lib/utils';
-import { Search } from 'lucide-react';
+import { Search, SearchIcon } from 'lucide-react';
 export interface SearchFormProps extends React.HTMLAttributes<HTMLFormElement> {
   searchURL: string;
   inputName: string;
@@ -20,17 +20,18 @@ const SearchForm = ({
       className={cn('w-full mx-auto flex space-x-4 justify-center', className)}
       {...props}
     >
-      <div className='w-96'>
+      <div className='w-3/4 relative'>
         <Input
           name={inputName}
-          className='w-full rounded-none  py-3 bg-white  text-white'
+          className='w-full rounded-none pl-8 border-2 py-3 bg-transparent text-white'
           placeholder='What are you looking for'
           type='search'
         />
+        <SearchIcon className='absolute w-4 h-5 left-2 my-auto  mx-0 right-0 top-0 bottom-0 text-white' />
       </div>
       <Button
-        variant={'secondary'}
-        className='right-0 top-0 h-full px-4'
+        variant={'default'}
+        className='right-0 top-0 h-full'
         type='submit'
       >
         <span className='hidden md:block'> Search</span>
