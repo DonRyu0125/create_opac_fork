af5e3b7905297b93f4b6cbdf389caaa79883e4a7
mikah13 <anhminhhoang13@gmail.com>
2024-02-15 02:57:24 -0800

    update func

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,5 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -21,7 +21,7 @@ export const copyToClipboard = (text: string): void => {
   try {
     Promise.resolve(navigator.clipboard.writeText(text));
   } catch (err) {
-    console.error("Failed to copy: ", err);
+    console.error('Failed to copy: ', err);
   }
 };
 
@@ -35,9 +35,36 @@ export const copyToClipboard = (text: string): void => {
 export const truncateWords = (
   text: string,
   maxLength: number = 20,
-  appendText: string = "...",
+  appendText: string = '...'
 ): string => {
   if (text.length <= maxLength) return text;
 
   return `${text.substring(0, maxLength)}${appendText}`;
 };
+
+type GenericObject = {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [key: string]: any;
+};
+
+export function deepSearchKey<T extends GenericObject>(
+  obj: T,
+  targetKey: string
+): unknown[] {
+  const result: unknown[] = [];
+
+  function search(obj: GenericObject, targetKey: string) {
+    for (const key in obj) {
+      if (key === targetKey) {
+        result.push(obj[key]);
+      } else if (typeof obj[key] === 'object' && obj[key] !== null) {
+        search(obj[key], targetKey);
+      }
+    }
+  }
+
+  search(obj, targetKey);
+  return result;
+}
+
+
