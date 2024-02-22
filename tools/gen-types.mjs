c30a5ef10a6f10a1d0a2097a2ae1cc932dbdb229
opacteam <mike@minisisinc.com>
2024-02-22 15:08:35 -0800

    refactor code in gen-types

--- tools/gen-types.mjs
+++ tools/gen-types.mjs
@@ -1,17 +1,4 @@
-import { compile, compileFromFile } from 'json-schema-to-typescript'
-
-// // compile from file
-// compileFromFile('foo.json')
-//     .then(ts => fs.writeFileSync('foo.d.ts', ts))
-
-// // or, compile a JS object
-// let mySchema = {
-//     properties: [...]
-// }
-// compile(mySchema, 'MySchema')
-//     .then(ts => ...)
-
-
+import { compileFromFile } from 'json-schema-to-typescript'
 import fs from "fs";
 import path, { resolve } from "path";
 import { getFiles } from "./index.mjs";
@@ -24,15 +11,14 @@ const outDir = resolve(base, "src/types");
 
 function main() {
     try {
-        console.log("Generating schemas .......");
+        console.log("Generating types .......");
         console.time("gen-types");
         const files = getFiles(inputDir, "json");
         files.forEach((file) => {
             const filePath = resolve(inputDir, file);
             generateTypeFile(filePath, outDir);
         });
-
-        console.log("Schemas successfully generated!");
+        console.log("Types successfully generated!");
         console.timeEnd("gen-types");
     } catch (error) {
         console.log(error);
@@ -54,31 +40,10 @@ function generateTypeFile(filePath = "./", outDir = "./types") {
             fs.mkdirSync(outDir);
         }
         const fileName = path.basename(filePath);
-        // const fileContent = fs.readFileSync(filePath, "utf8");
-        // console.log(fileContent)
 
         const typedPath = resolve(outDir, `${fileName}.d.ts`);
         compileFromFile(filePath)
             .then(ts => fs.writeFileSync(typedPath, ts))
-        // const fileName = path.basename(filePath);
-
-        // const json = JSON.parse(fileContent);
-        // const rootType = getType(json);
-        // const root = getStructure(rootType, fileName);
-        // traverseObject(json, root);
-        // const metadata = {
-        //     $schema: "http://json-schema.org/draft-07/schema#",
-        // };
-        // const schemaContent = { ...metadata, ...root };
-        // const schemaPath = `${outDir}/${fileName}`;
-        // if (!fs.existsSync(outDir)) {
-        //     fs.mkdirSync(outDir);
-        // }
-
-        // if (fs.existsSync(schemaPath)) {
-        //     fs.unlinkSync(schemaPath);
-        // }
-        // fs.writeFileSync(schemaPath, JSON.stringify(schemaContent));
 
     } catch (err) {
         throw new Error("File not found");
