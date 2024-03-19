055c52c8130adc8e5c5b05e0eaa4a3ebca87da4c
mike.hoang@ttt.studio <mike.hoang@ttt.studio>
2024-03-19 01:20:32 -0700

    format files

--- src/types/fields.json.d.ts
+++ src/types/fields.json.d.ts
@@ -5,22 +5,22 @@
  * and run json-schema-to-typescript to regenerate this file.
  */
 
-export type Database = string;
-export type Label = string;
-export type Name = string;
-export type Summary = boolean;
-export type Grid = boolean;
-export type Detail = boolean;
+export type Database = string
+export type Label = string
+export type Name = string
+export type Summary = boolean
+export type Grid = boolean
+export type Detail = boolean
 export type Items = {
-  label?: Label;
-  name?: Name;
-  summary?: Summary;
-  grid?: Grid;
-  detail?: Detail;
-  [k: string]: unknown;
-}[];
+	label?: Label
+	name?: Name
+	summary?: Summary
+	grid?: Grid
+	detail?: Detail
+	[k: string]: unknown
+}[]
 export type FieldsJson = {
-  database?: Database;
-  items?: Items;
-  [k: string]: unknown;
-}[];
+	database?: Database
+	items?: Items
+	[k: string]: unknown
+}[]
