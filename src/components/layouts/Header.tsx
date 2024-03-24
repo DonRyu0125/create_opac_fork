957ace72915a155057e582be66fa6f08983b6c8c
opacteam <mike@minisisinc.com>
2024-03-24 00:47:17 -0700

    update header style

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -14,7 +14,7 @@ const Header = () => {
 			<div className="flex h-16 items-center justify-between max-w-screen-xl mx-auto">
 				<div className="flex-1 md:flex md:items-center md:gap-12">
 					<a className="block text-teal-600" href="/">
-						<span className="sr-only">Home</span>
+						<span className="sr-only ">Home</span>
 						<img className="h-12" src={logo} alt="logo" />
 					</a>
 				</div>
@@ -23,9 +23,9 @@ const Header = () => {
 					<nav className="hidden md:block">
 						<ul className="flex items-center gap-6 text-sm ">
 							{navigations.map((nav) => (
-								<li key={nav.title}>
+								<li key={nav.title} className="hover:border-b-opac-green">
 									<Link
-										className="transition no-underline text-md text-opac-white hover:text-opac-green"
+										className="transition no-underline  text-lg text-opac-white hover:text-opac-green"
 										href={nav.url}>
 										{nav.title}
 									</Link>
