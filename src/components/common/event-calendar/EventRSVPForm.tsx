a8693452b610c2a47bcabe17c4346747cd2259df
donRyu0125 <don@minisisinc.com>
2024-04-03 11:29:20 -0700

    Add EventRSVPForm component

---
+++ src/components/common/event-calendar/EventRSVPForm.tsx
@@ -0,0 +1,15 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+
+const EventRSVPForm = () => {
+
+  return (
+    <div>
+      <Button>Register</Button>
+    </div>
+  )
+
+}
+
+export default EventRSVPForm
