aeea58eae6516790e5a1f5acbe306f1f047016c5
donRyu0125 <don@minisisinc.com>
2024-05-21 16:12:50 -0700

    edit cancel landing form

--- src/page/RSVPCancelLandingPage.tsx
+++ src/page/RSVPCancelLandingPage.tsx
@@ -37,8 +37,8 @@ type PatronInfo = {
 	occ2: string
 }
 
-const RSVP = () => {
-	const [loading, setLoading] = useState(false)
+const RSVPCancelLandingPage = () => {
+	const [loading, setLoading] = useState(true)
 	const [patronInfo, setPatronInfo] = useState<PatronInfo>({
 		TAG_FUNC_P_ATTND: '',
 		TAG_FUNC_P_FIRST: '',
@@ -180,86 +180,81 @@ const RSVP = () => {
 			})
 	}
 
+	const showRegister = () => {
+		return (
+			<div className="flex flex-1 items-center justify-center h-full">
+				{registerd ? (
+					<div className="h-full mx-auto px-4 py-10 text-center relative">
+						<h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+							This will cancel your registration for {patronInfo?.TAG_NAME}
+						</h1>
+						<div className="mt-4 text-gray-500 grid grid-cols-2 gap-1 text-lg">
+							<div className="text-left border-2 border-solid rounded-lg p-5 overflow-x-auto">
+								<div>{patronInfo?.TAG_NAME}</div>
+								<div>{patronInfo?.TAG_FUNC_DATE}</div>
+								<div>
+									{patronInfo?.TAG_FUNC_START_T} - {patronInfo?.TAG_FUNC_END_T}
+								</div>
+								<div>
+									{patronInfo?.BRANCH_ADDRESS}, Room: {patronInfo?.TAG_FUNC_ROOM}
+								</div>
+							</div>
+							<div className="text-left border-2 border-solid rounded-lg p-5 overflow-x-auto">
+								<div>
+									{patronInfo?.TAG_FUNC_P_LAST}, {patronInfo?.TAG_FUNC_P_FIRST}
+								</div>
+								<div>{patronInfo?.TAG_FUNC_P_EMAIL}</div>
+								<div>Registered: {patronInfo?.REGISTERED_DATE}</div>
+								<div className="border-2 border-dashed p-2">
+									{patronInfo?.TAG_FUNC_P_ATTND} spot reserved
+								</div>
+							</div>
+						</div>
+						<Button
+							onClick={onClick}
+							className="flex items-center justify-center w-[300px] h-[50px] mt-6 inline-block rounded bg-red-600 text-lg font-bold text-white hover:bg-indigo-700 focus:outline-none focus:ring">
+							<div>Unregister</div>
+						</Button>
+					</div>
+				) : (
+					<div className="mx-auto max-w-xl px-4 py-8 text-center">
+						<h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+							You are not in the list !
+						</h1>
+						<h2 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+							Please go to our website to register.
+						</h2>
+						<p className="mt-4 text-gray-500">Website name</p>
+					</div>
+				)}
+			</div>
+		)
+	}
+
 	return (
 		<Layout>
-			<div className="flex h-screen flex-col bg-white">
+			<div className="flex h-full flex-col bg-white">
 				<img
 					src="https://images.unsplash.com/photo-1558769132-cb1aea458c5e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1548&q=80"
 					alt=""
 					className="h-64 w-full object-cover"
 				/>
-				<div className="flex flex-1 items-center justify-center h-full ">
-					{registerd ? (
-						<>
-							{loading && (
-								<Spinner
-									height={'h-full'}
-									spinHeight={'h-20'}
-									spinWidth={'w-20'}
-									background={'bg-white'}
-								/>
-							)}
-							<div className="h-full mx-auto px-4 py-8 text-center relative">
-								<h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-									This will cancel your registration for {patronInfo?.TAG_NAME}
-								</h1>
-								<div className="mt-4 text-gray-500 grid grid-cols-2 gap-1 text-lg">
-									<div className="text-left border-2 border-solid rounded-lg p-5 overflow-x-auto">
-										<div>{patronInfo?.TAG_NAME}</div>
-										<div>{patronInfo?.TAG_FUNC_DATE}</div>
-										<div>
-											{patronInfo?.TAG_FUNC_START_T} -{' '}
-											{patronInfo?.TAG_FUNC_END_T}
-										</div>
-										<div>
-											{patronInfo?.BRANCH_ADDRESS}, Room:{' '}
-											{patronInfo?.TAG_FUNC_ROOM}
-										</div>
-									</div>
-									<div className="text-left border-2 border-solid rounded-md p-5 overflow-x-auto">
-										<div>
-											{patronInfo?.TAG_FUNC_P_LAST},{' '}
-											{patronInfo?.TAG_FUNC_P_FIRST}
-										</div>
-										<div>{patronInfo?.TAG_FUNC_P_EMAIL}</div>
-										<div>Registered: {patronInfo?.REGISTERED_DATE}</div>
-										<div className="border-2 border-dashed p-2">
-											{patronInfo?.TAG_FUNC_P_ATTND} spot reserved
-										</div>
-									</div>
-								</div>
-								<Button
-									onClick={onClick}
-									className="w-[300px] mt-6 inline-block rounded bg-red-600 px-5 py-3 text-sm font-bold text-white hover:bg-indigo-700 focus:outline-none focus:ring">
-									Unregister
-								</Button>
-							</div>
-						</>
-					) : (
-						<>
-							{loading && (
-								<Spinner
-									height={'h-full'}
-									spinHeight={'h-20'}
-									spinWidth={'w-20'}
-									background={'bg-white'}
-								/>
-							)}
-							<div className="mx-auto max-w-xl px-4 py-8 text-center">
-								<h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-									You are not in the list !
-								</h1>
-								<h2 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-									Please go to our website to register.
-								</h2>
-								<p className="mt-4 text-gray-500">Website name</p>
-							</div>
-						</>
-					)}
+				<div className={'min-h-[350px] flex items-center justify-center h-full'}>
+				{loading ? 
+					<Spinner
+						height={'h-full'}
+						spinHeight={'h-20'}
+						spinWidth={'w-20'}
+						background={'bg-white'}
+					/>
+					:
+					showRegister()
+				}
 				</div>
+				
 			</div>
 		</Layout>
 	)
 }
 
-export default RSVP
+export default RSVPCancelLandingPage
