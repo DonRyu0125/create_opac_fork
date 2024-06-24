2fa24e6156a759dec10fb207d362c802b9c57a09
donRyu0125 <don@minisisinc.com>
2024-06-24 11:42:22 -0700

    add bookmark index design

---
+++ src/page/bookmark/ViewBookmarks.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ViewBookmarks = () => {
+	return (
+		<>
+			<button className="flex h-10 w-full items-center justify-between rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground  disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1 hover:bg-gray-200">
+				Bookmark all records
+			</button>
+		</>
+	)
+}
+
+export default ViewBookmarks
--- src/page/summary/SummaryPageAction.tsx
+++ src/page/summary/SummaryPageAction.tsx
@@ -5,6 +5,7 @@ import useConstants from '@/hooks/useConstants'
 import useJSONData from '@/hooks/useJSONData'
 import { SummarySample } from '@/samples'
 import { Label } from '@radix-ui/react-dropdown-menu'
+import ViewBookmarks from '../bookmark/ViewBookmarks'
 
 /**
  * This component contains:
@@ -27,17 +28,22 @@ const SummaryPageAction = () => {
 				<Label>{message.sortBy}</Label>
 				<DropdownSelect title={message.sortBy} options={[]} />
 			</div>
-			<div className="flex flex-col space-y-2">
-				<div className={'flex'}><Label>{message.bookmark}<hr/></Label></div>
+			<div className="flex items-center">
+				<Label>{message.bookmark}</Label>
+				<div className="flex-grow border-t border-gray-600 ml-[4px]"></div>
 			</div>
-
+			<div className="flex flex-col space-y-2">
+				<ViewBookmarks/>
+				<button className="flex h-10 w-full items-center justify-between rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground  disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1 hover:bg-gray-200">Print this page</button>
+				<button className="flex h-10 w-full items-center justify-between rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground  disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1 hover:bg-gray-200">View bookmarks</button>
+			</div>
 			{filter && filter.length > 0 && (
 				<div className="flex flex-col space-y-2">
 					<Label>{message.filterBy}</Label>
 					<div className="flex flex-col space-y-4">
 						{filter.map((item, index) => (
 							<CollapseList title={item._title} expand={index === 0} key={item._name}>
-								<div className="space-y-3  border-t p-4">
+								<div className="space-y-3 border-t p-4">
 									{item.item_group.map((option) => (
 										<CheckboxWithLabel
 											callback={() => {
