591ed5de2657d094b255ff84e76d6b16959bd889
donRyu0125 <don@minisisinc.com>
2024-07-02 12:17:19 -0700

    generic component  for cancel and confirm for rsvp

--- src/page/RSVP/Cancellation.tsx
+++ src/page/RSVP/Cancellation.tsx
@@ -27,6 +27,7 @@ import axios from 'axios'
 import { useEffect, useState } from 'react'
 import useConstants from '@/hooks/useConstants'
 import {PatronInfo, STATUS_TYPE, initialPatronInfo} from '@/types/patroninfo'
+import { CancelTmp } from './ActionComponent'
 
 const RSVP_CANCEL = () => {
 	const [loading, setLoading] = useState(true)
@@ -250,49 +251,6 @@ const RSVP_CANCEL = () => {
 				)}
 			</div>
 		</Layout>
-	)
-}
-
-const CancelTmp = ({ patronInfo, onClick }: any) => {
-	const message: any = useConstants().message
-	return (
-		<div className="text-center">
-			<h1 className="text-l font-bold tracking-tight text-gray-900 sm:text-4xl">
-				{message.pleaseCancel} :
-			</h1>
-			<h2 className="text-l font-bold tracking-tight text-gray-900 sm:text-4xl">
-				{patronInfo?.TAG_NAME}
-			</h2>
-			<div className="mt-4 text-gray-500 sm:flex justify-evenly text-lg w-full">
-				<div className="sm:w-1/2 max-w-[500px] text-left border-2 border-solid rounded-lg p-5 mx-2">
-					<div>{patronInfo?.TAG_NAME}</div>
-					<div>{patronInfo?.TAG_FUNC_DATE}</div>
-					<div>
-						{patronInfo?.TAG_FUNC_START_T} - {patronInfo?.TAG_FUNC_END_T}
-					</div>
-					<div>{patronInfo?.BRANCH_ADDRESS}</div>
-					<div>
-						{message.room}: {patronInfo?.TAG_FUNC_ROOM}
-					</div>
-				</div>
-				<div className="sm:w-1/2 max-w-[500px] text-left border-2 border-solid rounded-lg p-5 mx-2">
-					<div>
-						{patronInfo?.TAG_FUNC_P_LAST}, {patronInfo?.TAG_FUNC_P_FIRST}
-					</div>
-					<div>{patronInfo?.TAG_FUNC_P_EMAIL}</div>
-					<div>{message.registered}:</div>
-					<div>{patronInfo?.TAG_FUNC_P_T}</div>
-					<div className="border-2 border-dashed p-2">
-						{patronInfo?.TAG_FUNC_P_ATTND} {message.spotReserved}
-					</div>
-				</div>
-			</div>
-			<Button
-				onClick={onClick}
-				className="flex items-center justify-center w-[300px] h-[50px] mt-6 inline-block rounded bg-red-600 text-lg font-bold text-white hover:bg-indigo-700 focus:outline-none focus:ring">
-				<div>{message.unregistered}</div>
-			</Button>
-		</div>
 	)
 }
 
--- src/page/RSVP/Confirmation.tsx
+++ src/page/RSVP/Confirmation.tsx
@@ -36,6 +36,7 @@ import { v4 as uuidv4 } from 'uuid'
 import { calNumOfPatron } from '@/components/common/event-calendar/EC-Util'
 import useConstants from '@/hooks/useConstants'
 import {PatronInfo, STATUS_TYPE, initialPatronInfo } from '@/types/patroninfo'
+import { ConfirmTmp } from './ActionComponent'
 
 const RSVP_CONFIRM = () => {
 	const message: any = useConstants().message
@@ -339,47 +340,5 @@ const RSVP_CONFIRM = () => {
 	)
 }
 
-const ConfirmTmp = ({ patronInfo, onClick }: any) => {
-	const message: any = useConstants().message
-	return (
-		<div className="text-center">
-			<h1 className="text-l font-bold tracking-tight text-gray-900 sm:text-4xl">
-				{message.pleaseConfirm}
-			</h1>
-			<h2 className="text-l font-bold tracking-tight text-gray-900 sm:text-4xl">
-				'{patronInfo?.TAG_NAME}'
-			</h2>
-			<div className="mt-4 text-gray-500 sm:flex justify-evenly text-lg w-full">
-				<div className="sm:w-1/2 text-left border-2 border-solid rounded-lg p-5 mx-2">
-					<div>{patronInfo?.TAG_NAME}</div>
-					<div>{patronInfo?.TAG_FUNC_DATE}</div>
-					<div>
-						{patronInfo?.TAG_FUNC_START_T} - {patronInfo?.TAG_FUNC_END_T}
-					</div>
-					<div>{patronInfo?.BRANCH_ADDRESS}</div>
-					<div>
-						{message.room}: {patronInfo?.TAG_FUNC_ROOM}
-					</div>
-				</div>
-				<div className="sm:w-1/2 text-left border-2 border-solid rounded-lg p-5 mx-2">
-					<div>
-						{patronInfo?.TAG_FUNC_P_LAST}, {patronInfo?.TAG_FUNC_P_FIRST}
-					</div>
-					<div>{patronInfo?.TAG_FUNC_P_EMAIL}</div>
-					<div>{message.registered}:</div>
-					<div>{patronInfo?.TAG_FUNC_P_T}</div>
-					<div className="border-2 border-dashed p-2">
-						{patronInfo?.TAG_FUNC_P_ATTND} {message.spotReserved}
-					</div>
-				</div>
-			</div>
-			<Button
-				onClick={onClick}
-				className="flex items-center justify-center w-[300px] h-[50px] mt-6 inline-block rounded bg-green-600 text-lg font-bold text-white hover:bg-indigo-700 focus:outline-none focus:ring">
-				<div>{message.confirm}</div>
-			</Button>
-		</div>
-	)
-}
 
 export default RSVP_CONFIRM
