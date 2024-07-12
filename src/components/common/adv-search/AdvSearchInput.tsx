848ee183b1edb17d0f757f354e9976f024a83141
donRyu0125 <don@minisisinc.com>
2024-07-11 17:15:20 -0700

    add index search field add delete func

--- src/components/common/adv-search/AdvSearchInput.tsx
+++ src/components/common/adv-search/AdvSearchInput.tsx
@@ -3,7 +3,7 @@ import { cn } from '@/lib/utils'
 import React, { useState } from 'react'
 import { SearchFormProps } from '../SearchForm'
 import useConstants from '@/hooks/useConstants'
-import { Menu } from 'lucide-react'
+import { ChevronDownIcon, Menu } from 'lucide-react'
 import { Button } from '@/components/ui/button'
 import {
 	Select,
@@ -11,26 +11,26 @@ import {
 	SelectItem,
 	SelectTrigger,
 	SelectValue,
-} from '@radix-ui/react-select'
+} from '@/components/ui/select'
 
-const AdvSearchInput = () => {
+const AdvSearchInput = ({updateField,index,exp}:any) => {
 	const { message, advancedSearch } = useConstants()
-	const [select, setSelect] = useState<string>();
+	const [select, setSelect] = useState<string>('Select a field')
 	return (
-		<div className="w-full flex relative m-2">
+		<div className="w-full flex relative m-2" key={index}>
 			<Select
 				onValueChange={(e) => {
 					setSelect(e)
 				}}>
 				<SelectTrigger className="w-72 border border-primary ">
-				{select}
+					<SelectValue className={'text-black'} placeholder={<div>{select}</div>} />
 				</SelectTrigger>
 				<SelectContent position={'popper'}>
 					{advancedSearch.map((item) => {
 						return (
 							<SelectItem
 								value={item.value}
-								className="w-52 border border-primary bg-neutral-300 ">
+								className="w-48 border border-primary bg-neutral-300 ">
 								{item.label}
 							</SelectItem>
 						)
@@ -38,7 +38,6 @@ const AdvSearchInput = () => {
 				</SelectContent>
 			</Select>
 			<Input
-				required
 				className={cn(
 					'w-full rounded-none pl-4 border py-3 bg-transparent border-primary '
 				)}
@@ -49,11 +48,11 @@ const AdvSearchInput = () => {
 				onValueChange={(e) => {
 					// setLanguage(e as LanguageCode)
 				}}>
-				<SelectTrigger className="w-16 border border-primary ">
+				<SelectTrigger className="w-32 border border-primary ">
 					<SelectValue placeholder={'And'} defaultValue={'And'} />
 				</SelectTrigger>
 				<SelectContent position={'popper'}>
-					<SelectItem value={'And'} className="w-14 border border-primary ">
+					<SelectItem value={'And'} className="w-20 border border-primary ">
 						And
 					</SelectItem>
 				</SelectContent>
--- src/page/AdvSearch/index.tsx
+++ src/page/AdvSearch/index.tsx
@@ -7,6 +7,8 @@ import { Label } from '@radix-ui/react-label'
 import PageHeader from '@/components/common/PageHeader'
 import { Button } from '@/components/ui/button'
 import useJSONData from '@/hooks/useJSONData'
+import { useState } from 'react'
+import { CircleMinus, Icon } from 'lucide-react'
 
 const AdvSearch = () => {
 	const { message, home } = useConstants()
@@ -14,34 +16,94 @@ const AdvSearch = () => {
 	const { searchURL: url } = home
 	const { session } = common
 
-	const getSearchURL = () => {
-		const domSessionId = document.querySelector('#session-id')?.textContent
-		if (document && domSessionId) {
-			return url.replace('/SCRIPTS/MWIMAIN.DLL', `${domSessionId}`)
-		}
-		if (session && session !== '') {
-			return url.replace('/SCRIPTS/MWIMAIN.DLL', `${session}`)
-		}
-		return url
+	const [searchExp, setSearchExp] = useState([
+		{ field: 'REFD', keyword: '', boolean: 'and' },
+		{ field: 'REFD', keyword: '', boolean: 'and' },
+		{ field: 'REFD', keyword: '' },
+	])
+
+	const updateField = (key, value, index) => {
+		const newSearchExp: any = [...searchExp]
+		newSearchExp[index][key] = value
+		setSearchExp(newSearchExp)
 	}
+
+	const removeField = (index) => {
+		const newSearchExp = [...searchExp]
+		newSearchExp.splice(index, 1)
+		if (newSearchExp.length > 0) {
+			newSearchExp[newSearchExp.length - 1].boolean = undefined
+		}
+		setSearchExp(newSearchExp)
+	}
+
+	const addField = () => {
+		const newSearchExp = [...searchExp]
+		newSearchExp[newSearchExp.length - 1].boolean = 'and'
+		newSearchExp.push({ field: 'REFD', keyword: '', boolean: 'and', remove: true })
+		setSearchExp(newSearchExp)
+	}
+
+	const resetFields = () => {
+		setSearchExp([
+			{ field: 'REFD', keyword: '', boolean: 'and' },
+			{ field: 'REFD', keyword: '', boolean: 'and' },
+			{ field: 'REFD', keyword: '' },
+		])
+	}
+
+	const submitSearch = () => {
+		// let data = searchExp.filter((e) => e.keyword !== "");
+		// let len = data.length;
+		// data = data
+		//   .map(
+		// 	(exp, index) =>
+		// 	  `${exp.field} ${exp.keyword} ${exp.boolean && index !== len - 1 ? exp.boolean : ""}`
+		//   )
+		//   .join(" ");
+		document.getElementById('advancedSearchInput').value = 'title ontario'
+		document.getElementById('advancedSearchForm').submit()
+	}
+
 	return (
 		<Layout>
 			<div
 				className={'w-full h-full min-h-[475px] flex flex-col justify-center items-center'}>
 				<PageHeader heading={'Advanced Search'} />
-				<form method="POST" action={getSearchURL()} className={'w-3/4 h-full '}>
-					<AdvSearchInput />
-					{/* <AdvSearchInput /> */}
-					<Button
-						variant={'default'}
-						className="right-0 top-0 h-full bg-opac-green"
-						type="submit">
-						{/* <span className="hidden md:block"> {message.searchButton}</span> */}
-						<span className=" block">
-							asd
-						</span>
-					</Button>
+				<form
+					method="POST"
+					id="advancedSearchForm"
+					action={`${url}&database=DESCRIPTION_WEB`}
+					className={'w-3/4 h-full '}>
+					<input name="QUERY_EXPRESSION" hidden id="advancedSearchInput" />
 				</form>
+				{searchExp.map((exp, index) => (
+					<>
+						<AdvSearchInput updateField={updateField} exp={exp} index={index} />
+						{index >= 3 ? (
+							<CircleMinus
+								className="dynamic-delete-button"
+								type="minus-circle-o"
+								onClick={(_) => {
+									removeField(index)
+								}}
+							/>
+						) : (
+							<span
+								className="dynamic-delete-button"
+								style={{ width: '24px' }}></span>
+						)}
+					</>
+				))}
+				<Button
+					variant={'default'}
+					className="right-0 top-0 h-full bg-opac-green"
+					onClick={submitSearch}>
+					<span className=" block">Search</span>
+				</Button>
+				<Button style={{ width: 200, marginTop: '40px' }} onClick={addField}>
+					Add field
+				</Button>
 			</div>
 		</Layout>
 	)
