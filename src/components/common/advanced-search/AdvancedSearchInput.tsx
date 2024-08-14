efaa1f9f5a9280bf1b4dcec62f0a49355f7a2b9a
donRyu0125 <don@minisisinc.com>
2024-08-14 16:49:01 -0700

    hotfix for adv search

--- src/components/common/advanced-search/AdvancedSearchInput.tsx
+++ src/components/common/advanced-search/AdvancedSearchInput.tsx
@@ -61,7 +61,7 @@ const AdvancedSearchInput = ({
 
 	const getLabel = () => {
 		let item = searchDatabase().items.find((item) => item.name === exp.field)
-		return item?.label
+		return ` (${item?.label ?? ' '}) `
 	}
 
 	return (
