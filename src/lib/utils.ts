2338959e2b8a8803e859fbf1398c65c39719d6a2
donRyu0125 <don@minisisinc.com>
2024-08-16 14:22:08 -0700

    add isLogin func

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -89,6 +89,22 @@ export const isDatePast = (dateString: string) => {
  * Cookie
  */
 
+export function isLogin() {
+	const cookies = document.cookie.split(';')
+	let hasId = false
+	let hasName = false
+	cookies.forEach((cookie) => {
+		const [name, value] = cookie.trim().split('=')
+		if (name === 'M2L_PATRON_ID' && value) {
+			hasId = true
+		}
+		if (name === 'M2L_PATRON_NAME' && value) {
+			hasName = true
+		}
+	})
+	return hasId && hasName
+}
+
 export function deleteCookie(cname: string) {
 	document.cookie = cname + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;'
 }
