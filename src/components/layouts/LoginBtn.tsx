579cc44503472cea26575d913fc37f980dbf179e
donRyu0125 <don@minisisinc.com>
2024-08-16 15:26:54 -0700

    edit lgoinBtn

--- src/components/layouts/LoginBtn.tsx
+++ src/components/layouts/LoginBtn.tsx
@@ -1,16 +1,19 @@
 import React, { useEffect, useState } from 'react'
 import Link from '../common/Link'
 import { clearCookies, getCookieValue, isLogin } from '@/lib/utils'
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from '../ui/dropdown-menu'
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from '../ui/dropdown-menu'
 import { ChevronDown } from 'lucide-react'
 import useConstants from '@/hooks/useConstants'
 
-// Se connecter
-// "url": "/scripts/mwimain.dll?logon&application=UNION_VIEW&language=144&file=[OPAC]login.html",
-// "title": "Se connecter"
-
 const LoginBtn = () => {
-	const { message } = useConstants()
+	const { message, config } = useConstants()
 	const [login, setlogin] = useState(false)
 	useEffect(() => {
 		setlogin(isLogin())
@@ -21,10 +24,13 @@ const LoginBtn = () => {
 			{login ? (
 				<DropdownMenu>
 					<DropdownMenuTrigger asChild className={'focus:outline-none focus:border-none'}>
-						<button className={'bg-red text-lg font-bold flex items-center max-w-[250px] truncate'}>
-							{message.welcome}, {getCookieValue('M2L_PATRON_NAME')}! <ChevronDown className={'h-4'}/>
+						<button
+							className={
+								'bg-red text-lg font-bold flex items-center max-w-[250px] truncate'
+							}>
+							{message.welcome}, {getCookieValue('M2L_PATRON_NAME')}!
+							<ChevronDown className={'h-4'} />
 						</button>
-						
 					</DropdownMenuTrigger>
 					<DropdownMenuContent align="end" className={'w-full'}>
 						<DropdownMenuLabel>{message.myAccount}</DropdownMenuLabel>
@@ -42,7 +48,7 @@ const LoginBtn = () => {
 					<Link
 						className="transition no-underline  text-lg text-opac-white hover:text-opac-green"
 						href={
-							'/scripts/mwimain.dll?logon&application=UNION_VIEW&language=144&file=[OPAC]login.html'
+							`${config.auth.url}`
 						}>
 						{message.logIn}
 					</Link>
