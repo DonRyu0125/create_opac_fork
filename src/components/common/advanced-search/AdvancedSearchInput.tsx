869e8a1c08f78dee4b44e6c08be0c0de96097af4
donRyu0125 <don@minisisinc.com>
2024-08-19 13:24:57 -0700

    edit the adv search

--- src/components/common/advanced-search/AdvancedSearchInput.tsx
+++ src/components/common/advanced-search/AdvancedSearchInput.tsx
@@ -108,7 +108,7 @@ const AdvancedSearchInput = ({
 						'absolute w-full h-full flex items-center justify-center text-gray-500 pointer-events-none'
 					}>
 					<span className="hidden lg:inline ml-[15px] text-gray-500 italic">
-						{message.searchPlaceholder}
+						{message.searchPlaceholderAdv}
 						{` ${getLabel()}`}
 					</span>
 				</div>
