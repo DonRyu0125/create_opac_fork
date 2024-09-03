f9833ecd10de82321807b965cbe7220fe8852610
opacteam <mike@minisisinc.com>
2024-09-03 08:03:56 -0700

    update: added dot env config

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,5 @@
 import client from 'axios'
 
 export const axios = client.create({
-	baseURL: 'http://localhost:3030',
+	baseURL: process.env.REACT_APP_API_ENDPOINT,
 })
-
-console.log(process.env.REACT_APP_API_ENDPOINT)
