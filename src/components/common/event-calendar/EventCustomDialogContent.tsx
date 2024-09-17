8e4d56c4fc13c027f9dae1d4603c95bbc3eb47d4
donRyu0125 <don@minisisinc.com>
2024-09-16 17:04:29 -0700

    rsvp carusel design

--- src/components/common/event-calendar/EventCustomDialogContent.tsx
+++ src/components/common/event-calendar/EventCustomDialogContent.tsx
@@ -22,7 +22,6 @@ import { Accessibility } from 'lucide-react'
 import EventRSVPForm from './EventRSVPForm'
 import EventRSVPCancel from './EventCancel'
 import useConstants from '@/hooks/useConstants'
-import ImageCarousel from '../ImageCarousel'
 import RSVPCarousel from './RSVPCarousel'
 
 // src: string
@@ -32,23 +31,22 @@ import RSVPCarousel from './RSVPCarousel'
 
 const images = [
 	{
+		type: 'Image',
 		src: 'https://media.britishmuseum.org/media/Repository/Documents/2014_10/6_16/ae91ce34_b9b4_44a4_9f0e_a3bc011460e8/preview_00433892_001.jpg',
 	},
 	{
+		type: 'Video',
 		src: 'https://titanapi.minisisinc.com/api/links/65043c1192f8422685fe7fcd57027d50/uuid/6d5353be50e04be7af42453ff65ca6c7/access',
-		video: 'https://titanapi.minisisinc.com/api/links/65043c1192f8422685fe7fcd57027d50/uuid/6d5353be50e04be7af42453ff65ca6c7/access',
-		caption: 'caption',
-		alt: 'ads',
 	},
 	{
+		type: 'Image',
 		src: 'https://media.britishmuseum.org/media/Repository/Documents/2014_11/2_1/5bcca073_26c7_4e0f_bb70_a3d7001a1c24/preview_01081548_001.jpg',
 	},
 	{
+		type: 'Image',
 		src: 'https://media.britishmuseum.org/media/Repository/Documents/2014_10/11_3/8dc66e9f_43e1_4170_956b_a3c1003d49ec/preview_00595099_001.jpg',
 	},
-	{
-		src: '',
-	},
+	{ type: '', src: '' },
 ]
 
 const EventCustomDialogContent = ({ elm, contactInfo, key }: any) => {
@@ -65,7 +63,6 @@ const EventCustomDialogContent = ({ elm, contactInfo, key }: any) => {
 					items={images}
 					renderItems={(image) => (
 						<img
-							alt={image.caption}
 							src={image.src}
 							className="h-36 mx-auto cursor-pointer object-cover border-4 hover:border-primary"
 						/>
--- src/components/common/event-calendar/RSVPCarousel.tsx
+++ src/components/common/event-calendar/RSVPCarousel.tsx
@@ -3,10 +3,8 @@ import { ChevronLeft, ChevronRight, Scan } from 'lucide-react'
 import LightboxImage from '../LightboxImage'
 
 export type ImageProps = {
+	type: string
 	src: string
-	alt?: string
-	caption?: string
-	video?: string
 }
 export interface ImageCarouselProps {
 	items: ImageProps[]
@@ -23,56 +21,44 @@ const VideoPlayer = ({ videoUrl }: { videoUrl: string }) => {
 }
 
 const RSVPCarousel = ({ items, renderItems }: ImageCarouselProps) => {
-    const [current, setCurrent] = React.useState(0)
-	const [openLightbox, setOpenLightbox] = React.useState(false)
+	const [current, setCurrent] = React.useState(0)
 	const currentMedia = items[current]
 
-  return (
-    <div className="flex flex-col space-y-4">
+	return (
+		<div className="flex flex-col space-y-4">
 			<div className="flex w-full group cursor-pointer relative">
 				<div className="w-full h-full min-h-[370px]">
-					{currentMedia.video ? (
-						<VideoPlayer videoUrl={currentMedia.video} />
+					{currentMedia.type !== 'Video' ? (
+						<>
+							<img
+								className="mx-auto w-full object-fill max-h-[370px]"
+								{...currentMedia}
+							/>
+							<div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 flex items-center justify-center transition-opacity duration-300 ease-in-out">
+								<h2 className="text-white text-xl">asd</h2>
+							</div>
+						</>
 					) : (
-						<img
-							className="mx-auto w-full object-fill max-h-[370px]"
-							{...currentMedia}
-						/>
-					)}
-				</div>
-				<Scan
-					strokeWidth={'3px'}
-					className="cursor-pointer absolute bg-gray-400 bg-opacity-30  w-8 h-8 text-white hover:text-primary bottom-2 right-2 transition-all ease-in duration-400"
-					onClick={() => setOpenLightbox(true)}
-				/>
-				<ChevronLeft
-					strokeWidth={'3px'}
-					className="cursor-pointer absolute bg-gray-400 bg-opacity-30 w-8 h-8 text-white hover:text-primary left-2 top-1/2  transition-all ease-in duration-400 "
-					onClick={() => setCurrent(current - 1 < 0 ? 0 : current - 1)}
-				/>
-				<ChevronRight
-					strokeWidth={'3px'}
-					className="cursor-pointer absolute bg-gray-400 bg-opacity-30  w-8 h-8 text-white hover:text-primary right-2 top-1/2 transition-all ease-in duration-400 "
-					onClick={() =>
-						setCurrent(current + 1 === items.length ? items.length - 1 : current + 1)
-					}
-				/>
-				<LightboxImage onOpen={setOpenLightbox} open={openLightbox} items={items} />
-			</div>
-			{/* {!isRSVP && (
-				<Slide
-					carouselStyle="sm:basis-1/4"
-					items={items}
-					renderItem={(item, index) => (
-						<div className="cursor-pointer w-fit" onClick={() => setCurrent(index)}>
-							{renderItems(item, index)}
+						<div className={'mx-11 my-11'}>
+							<VideoPlayer videoUrl={currentMedia.src} />
 						</div>
 					)}
-					auto={false}
-				/>
-			)} */}
+				</div>
+			</div>
+			<ChevronLeft
+				strokeWidth={'3px'}
+				className="cursor-pointer absolute bg-gray-400 bg-opacity-30 w-8 h-8 text-white hover:text-primary left-2 top-1/2 transform -translate-y-1/2 transition-all ease-in duration-400"
+				onClick={() => setCurrent(current - 1 < 0 ? 0 : current - 1)}
+			/>
+			<ChevronRight
+				strokeWidth={'3px'}
+				className="cursor-pointer absolute bg-gray-400 bg-opacity-30 w-8 h-8 text-white hover:text-primary right-2 top-1/2 transform -translate-y-1/2 transition-all ease-in duration-400"
+				onClick={() =>
+					setCurrent(current + 1 === items.length ? items.length - 1 : current + 1)
+				}
+			/>
 		</div>
-  )
+	)
 }
 
 export default RSVPCarousel
