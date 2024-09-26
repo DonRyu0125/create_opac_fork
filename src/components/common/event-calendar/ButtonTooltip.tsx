9b3bcdc51d65c57ab4322be2b66be155ae28f857
donRyu0125 <don@minisisinc.com>
2024-09-26 14:19:38 -0700

    change the tooltip component

--- src/components/common/event-calendar/ButtonTooltip.tsx
+++ src/components/common/event-calendar/ButtonTooltip.tsx
@@ -1,24 +1,25 @@
-import * as Tooltip from '@radix-ui/react-tooltip'
-import { TAG_NAME } from './Constants' 
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip'
+import { TAG_NAME } from './Constants'
 
 interface ButtonTooltipProps {
 	children: React.ReactNode
-	item: Array<{ [key: string]: string }> 
+	item: Array<{ [key: string]: string }>
 }
 
 const ButtonTooltip: React.FC<ButtonTooltipProps> = ({ children, item }) => {
 	return (
-		<Tooltip.Provider>
-			<Tooltip.Root>
-				<Tooltip.Trigger asChild>{children}</Tooltip.Trigger>
-				<Tooltip.Portal>
-					<Tooltip.Content side="top" align="center" sideOffset={5} className="hidden z-50 md:block max-w-[200px] max-h-[300px] overflow-y-hidden p-2 rounded bg-primary text-white text-start custom-scrollbar">
-						{item?.map((elm, key) => <div key={key}>&#x2022;{elm[TAG_NAME]}</div>)}
-						<Tooltip.Arrow className={'z-50 fill-primary'} width={5} height={5}/>
-					</Tooltip.Content>
-				</Tooltip.Portal>
-			</Tooltip.Root>
-		</Tooltip.Provider>
+		<TooltipProvider>
+			<Tooltip>
+				<TooltipTrigger asChild>{children}</TooltipTrigger>
+				<TooltipContent
+					side="top"
+					align="center"
+					sideOffset={5}
+					className="hidden z-50 md:block max-w-[250px] max-h-[250px] overflow-y-hidden p-2 rounded bg-primary text-white text-start custom-scrollbar">
+					{item?.map((elm, key) => <div key={key}>&#x2022;{elm[TAG_NAME]}</div>)}
+				</TooltipContent>
+			</Tooltip>
+		</TooltipProvider>
 	)
 }
 
