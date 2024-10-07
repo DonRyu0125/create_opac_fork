e20a28671d5690dbda65408dbc9a02b7c422f954
nwu01 <nixonwu01@gmail.com>
2024-10-07 13:22:34 -0700

    patronprofile change

--- src/page/PatronProfile.tsx
+++ src/page/PatronProfile.tsx
@@ -1,10 +1,12 @@
 // import PatronForm from '@/components/common/patron/PatronForm'
 import PatronLayout from '@/components/layouts/patron'
+import useJSONData from '@/hooks/useJSONData'
 const PatronProfile = () => {
-
+        const { common, pagination, backToSummary, data, records } = useJSONData({ selector: '#xml_record' })
+        console.log(records)
 	return (
                 <PatronLayout>
-                        
+                        Hi {records[0].first_name},
                 </PatronLayout>
 	)
 }
