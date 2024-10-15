10483b9541d6bd9b5376a2d912fc29a2bf934599
donRyu0125 <don@minisisinc.com>
2024-10-15 11:35:38 -0700

    conflix fix

--- src/page/login/Register.tsx
+++ src/page/login/Register.tsx
@@ -7,6 +7,7 @@ import { useForm } from 'react-hook-form'
 import useConstants from '@/hooks/useConstants'
 import { Button } from '@/components/ui/button'
 import axios from 'axios'
+import { CircleCheck } from 'lucide-react'
 
 type FormData = {
 	C_EMAIL: string
