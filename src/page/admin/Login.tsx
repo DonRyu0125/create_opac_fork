367da703cae707710ce7587ff2577a6ad022835f
opacteam <mike@minisisinc.com>
2024-11-03 21:44:05 -0800

    update move login form

--- src/page/admin/Login.tsx
+++ src/page/admin/Login.tsx
@@ -5,11 +5,6 @@ import { useAdminAuth } from '@/providers/AdminAuthProvider'
 import { useRef } from 'react'
 
 const AdminLogin = () => {
-	const { signIn } = useAdminAuth()
-	const usernameRef = useRef<HTMLInputElement>(null)
-	const passwordRef = useRef<HTMLInputElement>(null)
-	const username = usernameRef?.current?.value || ''
-	const password = passwordRef?.current?.value || ''
 	return (
 		<AdminLayout>
 			<div className="w-full max-w-3xl space-y-6 text-center mx-auto">
@@ -17,35 +12,46 @@ const AdminLogin = () => {
 					MINISIS Template Toolkit
 				</h1>
 
+				<AdminLoginForm />
+
 				<p className="text-lg text-slate-600">
 					Welcome to MTT. Please login with your provided credential to access your
 					account.
 				</p>
-
-				<form className="space-y-4 ">
-					<Input
-						ref={usernameRef}
-						className="max-w-xl mx-auto h-12 text-lg"
-						placeholder="Enter your Username"
-						type="text"
-						required
-					/>
-					<Input
-						ref={passwordRef}
-						className="max-w-xl mx-auto h-12 text-lg"
-						placeholder="Enter your Password"
-						type="password"
-						required
-					/>
-
-					<Button
-						onClick={() => signIn(username, password)}
-						className="h-12 px-8 text-lg text-white bg-[#0B2C4D] hover:bg-[#0B2C4D]/90">
-						Login
-					</Button>
-				</form>
 			</div>
 		</AdminLayout>
+	)
+}
+
+const AdminLoginForm = () => {
+	const { signIn } = useAdminAuth()
+	const usernameRef = useRef<HTMLInputElement>(null)
+	const passwordRef = useRef<HTMLInputElement>(null)
+	const username = usernameRef?.current?.value || ''
+	const password = passwordRef?.current?.value || ''
+	return (
+		<form className="space-y-4 ">
+			<Input
+				ref={usernameRef}
+				className="max-w-xl mx-auto h-12 text-lg"
+				placeholder="Enter your Username"
+				type="text"
+				required
+			/>
+			<Input
+				ref={passwordRef}
+				className="max-w-xl mx-auto h-12 text-lg"
+				placeholder="Enter your Password"
+				type="password"
+				required
+			/>
+
+			<Button
+				onClick={() => signIn(username, password)}
+				className="h-12 px-8 text-lg text-white bg-[#0B2C4D] hover:bg-[#0B2C4D]/90">
+				Login
+			</Button>
+		</form>
 	)
 }
 
