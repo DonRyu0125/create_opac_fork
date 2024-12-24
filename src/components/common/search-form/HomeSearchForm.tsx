ba0e219b12b1f90172db29d5f16044fd66b32eac
mikah13 <anhminhhoang13@gmail.com>
2024-12-24 03:12:15 -0800

    update search form

--- src/components/common/search-form/HomeSearchForm.tsx
+++ src/components/common/search-form/HomeSearchForm.tsx
@@ -19,22 +19,22 @@ export default function HomeSearchForm({
 }: HomeSearchFormProps) {
 	const { message } = useConstants()
 	return (
-		<Card className="w-full max-w-3xl mx-auto rounded-md">
+		<Card className="w-full max-w-3xl mx-auto rounded-md shadow-lg">
 			<CardHeader>
 				<CardTitle className="text-2xl font-bold text-left">
 					Search all collections
 				</CardTitle>
 			</CardHeader>
 			<CardContent className="space-y-4">
-				<form method="POST" action={action} className="flex space-x-2 mb-4">
+				<form method="POST" action={action} className="flex space-x-2 mb-4 relative">
 					<Input
-						className="flex-grow"
+						className="w-full h-14 pl-6 pr-32 text-lg border-2 border-gray-200 focus:border-primary "
 						required
 						name={inputName || 'KEYWORD_CLUSTER'}
 						placeholder={message.searchPlaceholder}
 						type="search"
 					/>
-					<Button className="bg-primary rounded-md ">
+					<Button className="absolute right-2 top-2 h-10 px-8 rounded-full bg-primary">
 						<span className="block">
 							<Search className="w-4 h-4 mr-6" />
 						</span>
