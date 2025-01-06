74dd7a0b3386bdbc5298922de0e75d2e0bb6ca79
mikah13 <anhminhhoang13@gmail.com>
2025-01-06 03:46:10 -0800

    update no record page

--- src/page/NoRecord.tsx
+++ src/page/NoRecord.tsx
@@ -1,28 +1,22 @@
-import Hero from '@/components/common/Hero'
-import HomeSearchForm from '@/components/common/search-form/HomeSearchForm'
+import Link from '@/components/common/Link'
 import Layout from '@/components/layouts'
 import useConstants from '@/hooks/useConstants'
-import { getSearchURL } from '@/lib/utils'
-import { UNION_SEARCH_CL } from './Home'
 
 const NoRecord = () => {
-	const { home, message } = useConstants()
-	const { heroBanner, searchURL } = home
+	const { message } = useConstants()
 	return (
 		<Layout>
-			<Hero
-				className=""
-				title={message.noResultFound}
-				backgroundImage={heroBanner}
-				description="">
-				<div className={'w-full mx-auto flex space-x-4 justify-center mt-6 max-w-2xl'}>
-					<HomeSearchForm
-						title="Search all collections"
-						inputName={UNION_SEARCH_CL}
-						action={getSearchURL(searchURL)}
-					/>
+			<div className="flex h-screen flex-col bg-white">
+				<div className="flex flex-1 items-center justify-center">
+					<div className="mx-auto max-w-xl px-4 py-8 text-center">
+						<h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+							{message.noResultFound}
+						</h1>
+
+						<Link href="/">Go Back Home</Link>
+					</div>
 				</div>
-			</Hero>
+			</div>
 		</Layout>
 	)
 }
