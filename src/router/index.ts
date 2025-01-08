cde01270d3c0794572c29826ff7226d755d6df1b
nwu01 <nixonwu01@gmail.com>
2025-01-07 16:36:20 -0800

    Client Login - Error Handling pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -43,6 +43,7 @@ import EnquiryConfirmed from '@/page/enquiry/EnquiryConfirmed'
 import ReproductionConfirmed from '@/page/reproduction/ReproductionConfirmed'
 import ReproductionForm from '@/page/reproduction/ReproductionForm'
 import ReproductionDetail from '@/page/reproduction/ReproductionDetail'
+import ClientLoginError from '@/page/login/ClientLoginError'
 
 export type TRoute = Record<string, (props?: any) => JSX.Element>
 
@@ -97,6 +98,7 @@ export const ROUTES: TRoute = {
 	reproductionform: ReproductionForm,
 	reproductionconfirmed: ReproductionConfirmed,
 	reproductiondetail: ReproductionDetail,
+	'client-login-error': ClientLoginError,
 	...ADMIN_ROUTES,
 }
 
