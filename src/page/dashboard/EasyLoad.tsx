fddd83032cbee79beac50fefcd9edbf8d74b106e
mikah13 <anhminhhoang13@gmail.com>
2025-01-14 02:27:52 -0800

    update route and easyload

---
+++ src/page/dashboard/EasyLoad.tsx
@@ -0,0 +1,13 @@
+import PatronLayout from '@/components/layouts/patron'
+
+const EasyLoad = () => {
+	return (
+		<PatronLayout>
+			<div className="container flex flex-col gap-8 p-6">
+				<div className="flex flex-wrap gap-2 sm:gap-4"></div>
+			</div>
+		</PatronLayout>
+	)
+}
+
+export default EasyLoad
