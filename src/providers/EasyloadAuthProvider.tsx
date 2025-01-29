075630bbcb6ae592c376f660cc46e532c517e31d
donRyu0125 <don@minisisinc.com>
2025-01-29 15:56:29 -0800

    hotfix

--- src/providers/EasyloadAuthProvider.tsx
+++ src/providers/EasyloadAuthProvider.tsx
@@ -7,9 +7,9 @@ const easyloadUserAtom = atom(localStorage.getItem('easyloadUser'))
 const loadingAtom = atom(false)
 const errorAtom = atom(null)
 const TENANT =
-	process.env.REACT_APP_EASYLOAD_TENANT 
+	process.env.REACT_APP_EASYLOAD_TENANT || import.meta.env.VITE_REACT_APP_EASYLOAD_TENANT
 const PASSWORD =
-	process.env.REACT_APP_EASYLOAD_PASSWORD 
+	process.env.REACT_APP_EASYLOAD_PASSWORD || import.meta.env.VITE_REACT_APP_EASYLOAD_PASSWORD
 
 type AuthContextType = {
 	authenticate: () => Promise<void>
