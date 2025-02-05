82118947f44ad11e11c43b5bd92ef6b0cbc27a06
donRyu0125 <don@minisisinc.com>
2025-02-05 11:45:49 -0800

    timeline hot fix

--- src/components/common/interactive-timeline/index.tsx
+++ src/components/common/interactive-timeline/index.tsx
@@ -74,7 +74,7 @@ const Timeline = () => {
 			let centuryLabel;
 
 			if (timeIndex >= 10000) {
-				const century = Math.floor((timeIndex - 10000) / 1000) * 1000
+				const century = Math.floor((timeIndex - 10000) / 500) * 500
 				centuryLabel = century === 0 ? 'AD 0' : `AD ${century}`
 			} else {
 				const offset = 10000 - timeIndex
