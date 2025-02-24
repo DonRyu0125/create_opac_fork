6ad41a2b9c1996a785264d14307657bc0469fb50
mikah13 <anhminhhoang13@gmail.com>
2025-02-24 11:23:53 -0800

    update

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -80,7 +80,7 @@ const Home = ({ previewMode = false, previewData }: MainPageProps) => {
 			<Hero className="" title={heading} backgroundImage={heroBanner} description="">
 				<div className={'w-full mx-auto flex space-x-4 justify-center mt-6 max-w-2xl'}>
 					<HomeSearchForm
-						title="Search all collections"
+						title={message.searchAllCollections}
 						inputName={UNION_SEARCH_CL}
 						action={getSearchURL(searchURL)}
 					/>
