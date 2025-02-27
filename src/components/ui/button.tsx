8574dd756e79fc711de7fb22ed7a4144d8876092
mikah13 <anhminhhoang13@gmail.com>
2025-02-27 10:45:06 -0800

    update

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,12 +9,12 @@ const buttonVariants = cva(
 	{
 		variants: {
 			variant: {
-				default: 'bg-primary text-primary-foreground hover:bg-[#C8102E]',
+				default: 'bg-primary text-primary-foreground hover:bg-primary/80',
 				destructive: 'bg-destructive text-destructive-foreground hover:bg-destructive/90',
 				outline:
 					'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
 				secondary: 'bg-secondary text-secondary-foreground hover:bg-secondary/80',
-				ghost: 'hover:bg-[#C8102E] hover:text-accent-foreground',
+				ghost: 'hover:bg-primary/80 hover:text-accent-foreground',
 				link: 'text-primary underline-offset-4 hover:underline',
 				danger: 'bg-red-600 text-white hover:bg-red-700',
 			},
