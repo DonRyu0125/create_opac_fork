ef5fa1300cbd3c326d5936b3cead2229ff567d4b
donRyu0125 <don@minisisinc.com>
2025-03-17 15:06:07 -0700

    add login btn at mobile view

--- src/components/layouts/MobileMenu.tsx
+++ src/components/layouts/MobileMenu.tsx
@@ -26,11 +26,11 @@ const MobileMenu = () => {
 						<SheetTitle>{siteName}</SheetTitle>
 					</SheetHeader>
 					<nav className="">
-						<ul className="flex flex-col items-center  text-md">
+						<ul className="flex flex-col items-center text-md">
 							{navigations.map((nav) => (
 								<li
 									key={nav.displayTitle}
-									className="flex items-center text-left h-12 w-full px-2  hover:text-white hover:bg-primary/60 ">
+									className="flex items-center text-left h-12 w-full px-2 hover:text-white hover:bg-primary/60 ">
 									<Link
 										className="transition no-underline text-md text-primary"
 										href={nav.linkURL}>
@@ -38,12 +38,14 @@ const MobileMenu = () => {
 									</Link>
 								</li>
 							))}
-							{auth.login && (
-								<li className="flex items-center text-left h-12 w-full px-2  hover:text-white hover:bg-primary/60">
-									<LoginBtn className="text-primary text-md " />
-								</li>
-							)}
 						</ul>
+						<div className="px-2 mb-3">
+							{auth.login && (
+								<div className=" inline-block bg-primary p-2 ring-offset-background rounded-lg" style={{borderRadius: "5px"}}>
+									<LoginBtn />
+								</div>
+							)}
+						</div>
 						<div className="px-2">
 							<LanguageSelect />
 						</div>
