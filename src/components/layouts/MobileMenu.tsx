8134fb757767aa8f46fa2e62c102edb51aaa5eb5
donRyu0125 <don@minisisinc.com>
2025-03-18 11:39:11 -0700

    mobile menu login change

--- src/components/layouts/MobileMenu.tsx
+++ src/components/layouts/MobileMenu.tsx
@@ -25,6 +25,15 @@ const MobileMenu = () => {
 					<SheetHeader>
 						<SheetTitle>{siteName}</SheetTitle>
 					</SheetHeader>
+					{auth.login && (
+						<div className={'flex justify-center items-center py-5'}>
+							<div
+								className=" inline-block bg-primary p-2 ring-offset-background rounded-lg"
+								style={{ borderRadius: '5px' }}>
+								<LoginBtn />
+							</div>
+						</div>
+					)}
 					<nav className="">
 						<ul className="flex flex-col items-center text-md">
 							{navigations.map((nav) => (
@@ -39,17 +48,10 @@ const MobileMenu = () => {
 								</li>
 							))}
 						</ul>
-						<div className="px-2 mb-3">
-							{auth.login && (
-								<div className=" inline-block bg-primary p-2 ring-offset-background rounded-lg" style={{borderRadius: "5px"}}>
-									<LoginBtn />
-								</div>
-							)}
-						</div>
-						<div className="px-2">
-							<LanguageSelect />
-						</div>
 					</nav>
+					<div className="px-2 absolute bottom-[23px] right-[14px]">
+						<LanguageSelect />
+					</div>
 				</SheetContent>
 			</Sheet>
 		</div>
