740619590404d100984de9147d99a35de1350954
donRyu0125 <don@minisisinc.com>
2025-03-24 10:30:07 -0700

    remove border b for map and timeline

--- src/components/common/interactive-map/index.tsx
+++ src/components/common/interactive-map/index.tsx
@@ -495,16 +495,15 @@ const InteractiveMap = ({ page }: { page: string }) => {
 												<a
 													href={`/SCRIPTS/MWIMAIN.DLL?UNIONSEARCH&SIMPLE_EXP=Y&KEEP=Y&ERRMSG=[MESSAGES]no-record.html&APPLICATION=UNION_VIEW&DATABASE=${library.database_name}&language=144&REPORT=WEB_UNION_DETAIL&EXP=ACCESSION_NUMBER%20${marker.ACCESSION_NUMBER}`}
 													target="_blank">
-													<h3 className="text-lg font-bold text-black  border-b pb-2">
+													<h3 className="text-lg font-bold text-black  pb-2">
 														{marker.ALL_TITLE_WORD_OCCURRENCE ?? 'n/a'}
 													</h3>
-
 													{marker?.IMAG_URL && (
 														<div className="bg-slate-100 h-48 mb-4 rounded-[14px]">
 															<img
 																src={getImage(marker.IMAG_URL)}
 																alt="Library"
-																className="w-full h-full object-contain  rounded-[14px]"
+																className="w-full h-full object-contain rounded-[14px]"
 															/>
 														</div>
 													)}
@@ -581,10 +580,9 @@ const InteractiveMap = ({ page }: { page: string }) => {
 												<a
 													href={`/SCRIPTS/MWIMAIN.DLL?UNIONSEARCH&SIMPLE_EXP=Y&KEEP=Y&ERRMSG=[MESSAGES]no-record.html&APPLICATION=UNION_VIEW&DATABASE=${archives.database_name}&language=144&REPORT=WEB_UNION_DETAIL&EXP=refd%20${marker.REFD}`}
 													target="_blank">
-													<h3 className="text-lg font-bold text-black  border-b pb-2">
+													<h3 className="text-lg font-bold text-black   pb-2">
 														{marker.TITLE ?? 'n/a'}
 													</h3>
-
 													{marker?.IMAG_URL && (
 														<div className="bg-slate-100 h-48 mb-4 rounded-[14px]">
 															<img
@@ -652,13 +650,13 @@ const InteractiveMap = ({ page }: { page: string }) => {
 										]}
 										icon={icons['museum']}>
 										<Popup
-											className="hidden md:block border-minisis-museum  border-2 rounded-[14px]"
+											className="hidden md:block border-minisis-museum border-2 rounded-[14px]"
 											offset={[2, 0]}>
 											<div className="w-[300px]">
 												<a
 													href={`/SCRIPTS/MWIMAIN.DLL?UNIONSEARCH&SIMPLE_EXP=Y&KEEP=Y&ERRMSG=[MESSAGES]no-record.html&APPLICATION=UNION_VIEW&DATABASE=${museum.database_name}&language=144&REPORT=WEB_UNION_DETAIL&EXP=ACCESSION_NUMBER%20${marker.ACCESSION_NUMBER}`}
 													target="_blank">
-													<h3 className="text-lg font-bold text-black border-b pb-2 overflow-x-auto">
+													<h3 className="text-lg font-bold text-black pb-2 overflow-x-auto">
 														{marker.TITLE ?? 'n/a'}
 													</h3>
 													{marker?.IMAG_URL && (
