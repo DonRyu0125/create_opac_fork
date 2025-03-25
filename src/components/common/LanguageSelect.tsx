545de92a9da934247d8c2e6c27c45a8afbdf4e5d
mikah13 <anhminhhoang13@gmail.com>
2025-03-25 09:27:52 -0700

    update flag

--- src/components/common/LanguageSelect.tsx
+++ src/components/common/LanguageSelect.tsx
@@ -38,14 +38,14 @@ export function LanguageSelect() {
 			onValueChange={(e) => {
 				setLanguage(e as LanguageCode)
 			}}>
-			<SelectTrigger className="w-auto bg-primary">
+			<SelectTrigger className="w-auto bg-primary text-primary">
 				<SelectValue placeholder={language.abbr} />
 			</SelectTrigger>
 			<SelectContent>
 				{LANGUAGE_ITEMS.map((item) => (
 					<SelectItem key={item.code} value={item.code} className="">
 						<div className="flex justify-center items-center">
-							<span className="ml-2 ">{item.abbr}</span>
+							<span className="">{item.abbr}</span>
 						</div>
 					</SelectItem>
 				))}
