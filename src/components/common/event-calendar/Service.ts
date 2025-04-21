20cb6122c9bf73360095b7f3d6628b5340b0d757
donRyu0125 <don@minisisinc.com>
2025-04-21 11:11:00 -0700

    edit calendar location to bk processing

--- src/components/common/event-calendar/Service.ts
+++ src/components/common/event-calendar/Service.ts
@@ -78,13 +78,14 @@ export const fetch_get = async (currentDate: Date, isWeekType?: boolean) => {
 
 export const getLocation = async () => {
 	const response = await axios.get(
-		`/scripts/mwimain.dll/144/${SUB_MWI_APPLICATION}/${LOCATION_REPORT}?commandsearch&exp=%2B%2B%40`, // ++@
+		`/preprocessing/CALENDAR_LOCATION.html`,
 		{
 			headers: {
 				'Content-Type': 'text/xml',
 			},
 		}
 	)
+	
 	const jsonData: any = convertXMLToJson(response.data)
 	return jsonData?.xml?.[LIBRARY_LOCATION_XML_TAG] ?? []
 }
