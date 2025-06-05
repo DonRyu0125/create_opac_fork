13729744ee4773b8e4aa8fe090d82b12ad3761bd
donRyu0125 <don@minisisinc.com>
2025-06-05 11:24:13 -0700

    request later reponsive

--- src/page/request-later/index.tsx
+++ src/page/request-later/index.tsx
@@ -10,6 +10,8 @@ import { endOfMonth, subMonths } from 'date-fns'
 import { DatePickerWithRange } from '@/components/common/client-profile/DatePickerWithRange'
 import { Calendar } from '@/components/ui/calendar'
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
+import dummy from './dummy.json'
+import useConstants from '@/hooks/useConstants'
 
 const RequestLater = () => {
 	const { backToSummary, records, getMedia, common } = useJSONData({ selector: '#xml_record' })
@@ -18,8 +20,18 @@ const RequestLater = () => {
 		event.preventDefault()
 		window.history.back()
 	}
-	const [selected, setSelected] = useState<Date>()
-	const [time, setTime] = useState('')
+	const { message } = useConstants()
+	const [selectDate, setSelectDate] = useState<{
+		date: Date
+		timeSlots: {
+			start_time?: string
+			end_time?: string
+			sp_start_time?: string
+			sp_end_time?: string
+		}[]
+	} | null>(null)
+	const [time, setTime] = useState()
+	const data = dummy.calendar_info
 
 	const weekdayToIndex: any = {
 		su: 0,
@@ -31,55 +43,11 @@ const RequestLater = () => {
 		sa: 6,
 	}
 
-	const operation_day_entry = [
-		{ weekday: 'mo', collection_time_entry: [{ start_time: '10:00', end_time: '10:59' }] ,date_closed: 'X'},
-		{ weekday: 'tu', collection_time_entry: [{ start_time: '10:00', end_time: '10:59' }] },
-		{ weekday: 'we', collection_time_entry: [{ start_time: '10:00', end_time: '10:59' }] },
-		{ weekday: 'th', collection_time_entry: [{ start_time: '10:00', end_time: '10:59' }] },
-		{ weekday: 'fr', collection_time_entry: [{ start_time: '10:00', end_time: '10:59' }] },
-		{ weekday: 'sa', date_closed: 'X' },
-		{ weekday: 'su', date_closed: 'X' },
-	]
-
-	const closure_date_entry = [
-		{ closure_date: '2025-06-07', closure_desc: 'Canada Day' },
-		{ closure_date: '2025-06-04', closure_desc: 'B.C. Day' },
-		{ closure_date: '2025-09-01', closure_desc: 'Labour Day' },
-		{ closure_date: '2025-09-30', closure_desc: 'National Day for Truth and Reconciliation' },
-		{ closure_date: '2025-10-13', closure_desc: 'Thanksgiving Day' },
-		{ closure_date: '2025-11-11', closure_desc: 'Remembrance Day' },
-		{ closure_date: '2025-12-25', closure_desc: 'Christmas Day' },
-		{ closure_date: '2025-12-26', closure_desc: 'Boxing Day' },
-	]
-
-	const sp_open_date_entry = [
-		{
-			open_date: '2025-06-07',
-			sp_open_collection_time_entry: [
-				{ sp_start_time: '10:00', sp_end_time: '10:59' },
-				{ sp_start_time: '11:00', sp_end_time: '11:59' },
-			],
-			sp_cutoff_date: '2025-06-06',
-			sp_cufoff_time: '12:00',
-		},
-		{
-			open_date: '2025-06-28',
-			sp_open_collection_time_entry: [
-				{ sp_start_time: '10:00', sp_end_time: '10:59' },
-				{ sp_start_time: '11:00', sp_end_time: '11:59' },
-			],
-			sp_cutoff_date: '2025-06-06',
-			sp_cufoff_time: '12:00',
-		},
-	]
-
-	const openWeekdays = operation_day_entry
+	const openWeekdays = data.operation_day_entry
 		.filter((d) => d.date_closed !== 'X')
 		.map((d) => weekdayToIndex[d.weekday])
-
-	const closureDates = new Set(closure_date_entry.map((d) => d.closure_date))
-
-	const specialOpenDates = new Set(sp_open_date_entry.map((d) => d.open_date))
+	const closureDates = new Set(data.closure_date_entry.map((d) => d.closure_date))
+	const specialOpenDates = new Set(data.sp_open_date_entry.map((d) => d.open_date))
 
 	function isOpen(date: Date) {
 		const yyyyMMdd = date.toISOString().split('T')[0]
@@ -88,12 +56,29 @@ const RequestLater = () => {
 		return openWeekdays.includes(date.getDay())
 	}
 
+	const handleSelect = (date: Date | undefined) => {
+		if (!date) return
+
+		const yyyyMMdd = date.toISOString().split('T')[0]
+		const sp = data.sp_open_date_entry.find((d) => d.open_date === yyyyMMdd)
+		if (sp) {
+			setSelectDate({ date, timeSlots: sp.sp_open_collection_time_entry })
+			return
+		}
+		const day = date.getDay()
+		const weekdayKey = Object.keys(weekdayToIndex).find((key) => weekdayToIndex[key] === day)
+		const op = data.operation_day_entry.find(
+			(d) => d.weekday === weekdayKey && d.date_closed !== 'X'
+		)
+
+		setSelectDate({ date, timeSlots: op?.collection_time_entry || [] })
+	}
+
 	return (
 		<Layout>
 			<section>
 				<div className="bg-gray-50 min-h-screen py-10">
 					<div className="max-w-4xl mx-auto bg-white shadow-md rounded-md p-6">
-						{/* Header */}
 						<div className="flex justify-between items-center border-b pb-4">
 							<form
 								method="post"
@@ -174,87 +159,105 @@ const RequestLater = () => {
 									<CircleEllipsis className="mr-2" />
 									Request Order Later
 								</h1>
-								<div className="py-4 [&_p]:my-4 [&_b]:text-lg [&_b]:underline">
-									<p>
-										You have requested to view <b>{reqData.req_item_title}</b>{' '}
-										with the reference number: <b>{reqData.req_item_id}</b>
+
+								<div className="mt-3 border p-4 rounded">
+									<div className="flex flex-row items-center">
+										<Archive className="mr-2" />
+										<h1 className="text-xl font-bold">Archives</h1>
+									</div>
+									<p className="text-lg font-bold mt-2">
+										{reqData.req_item_title}
 									</p>
-									<p>
-										Please confirm whether you would like to proceed with this
-										request.
+									<p className="text-sm text-gray-600">
+										Reference Number: {reqData.req_item_id}
 									</p>
 								</div>
 
-								<div>
-									<div className="border p-4 rounded">
-										<div className="flex flex-row items-center">
-											<Archive className="mr-2" />
-											<h1 className="text-xl font-bold">Archives</h1>
-										</div>
-										<p className="text-lg font-bold mt-2">
-											{reqData.req_item_title}
-										</p>
-										<p className="text-sm text-gray-600">
-											Reference Number: {reqData.req_item_id}
-										</p>
-									</div>
+								<div className="py-4 [&_p]:my-4 [&_b]:text-lg [&_b]:underline">
+									<p>
+										This request requires that you visit archive in
+										person to see the item. Items can only be requested a
+										maximum of two weeks in advance of your visit.
+									</p>
 								</div>
 								<div className="flex items-center text-xl font-bold mt-7">
 									<CircleEllipsis className="mr-2" />
 									Please select the date of your visit
 								</div>
 								<div className="py-4 [&_p]:my-4 [&_b]:text-lg [&_b]:underline">
-									<p>Your request will be fulfilled in three business days.</p>
 									<p>
-										Please confirm whether you would like to proceed with this
-										request.
+										The item will typically be ready to use in the Archives
+										Study Area within an hour of your selected time (e.g.
+										documents in the 11:00 collection are usually ready for use
+										by 12:00).
 									</p>
 								</div>
 								<div className="w-full md:flex ">
 									<div className="md:w-1/2 flex justify-center">
 										<Calendar
 											mode="single"
-											selected={selected}
-											onSelect={setSelected}
+											selected={selectDate?.date}
+											onSelect={handleSelect}
 											disabled={(date) => !isOpen(date)}
 										/>
 									</div>
-									<div className="md:w-1/2">
-										<div className="text-lg font-bold mt-2">Selected Date</div>
-										<Input
-											className="inline-flex items-center justify-between px-4 py-2 border rounded bg-white shadow text-sm w-40"
-											value={
-												selected ? selected.toISOString().split('T')[0] : ''
-											}
-											readOnly
-										/>
-										<div className="text-lg font-bold mt-2">Select a Time</div>
-										<DropdownMenu.Root>
-											<DropdownMenu.Trigger className="inline-flex items-center justify-between px-4 py-2 border rounded bg-white shadow text-sm w-40">
-												{time}
-												<ChevronDownIcon className="h-4 w-4" />
-											</DropdownMenu.Trigger>
-											<DropdownMenu.Portal>
-												<DropdownMenu.Content
-													sideOffset={5}
-													className="rounded-md bg-white shadow-md border p-1 text-sm w-40">
-													{['Profile', 'Settings'].map((item: any) => (
-														<DropdownMenu.Item
-															key={item}
-															className="px-3 py-2 rounded hover:bg-gray-100 cursor-pointer"
-															onSelect={() => setTime(item)}>
-															{item}
-														</DropdownMenu.Item>
-													))}
-												</DropdownMenu.Content>
-											</DropdownMenu.Portal>
-										</DropdownMenu.Root>
+									<div className="md:w-1/2 flex justify-center md:block">
+										<div className={'mx-2 md:mx-0'}>
+											<div className="text-lg font-bold mt-2">
+												Select Date
+											</div>
+											<input
+												className="inline-flex items-center justify-between px-4 py-2 border rounded bg-white shadow text-sm w-40"
+												value={
+													selectDate
+														? selectDate.date
+																.toISOString()
+																.split('T')[0]
+														: ''
+												}
+												readOnly
+											/>
+										</div>
+										<div className={'mx-2 md:mx-0'}>
+											<div className="text-lg font-bold mt-2">
+												Select a Time
+											</div>
+											<DropdownMenu.Root>
+												<DropdownMenu.Trigger className="inline-flex items-center justify-between px-4 py-2 border rounded bg-white shadow text-sm w-40">
+													{time || 'Select'}
+													<ChevronDownIcon className="h-4 w-4" />
+												</DropdownMenu.Trigger>
+												<DropdownMenu.Portal>
+													<DropdownMenu.Content
+														sideOffset={5}
+														className="rounded-md bg-white shadow-md border p-1 text-sm w-40">
+														{selectDate?.timeSlots.map(
+															(item: any, idx) => (
+																<DropdownMenu.Item
+																	key={idx}
+																	className="px-3 py-2 rounded hover:bg-gray-100 cursor-pointer"
+																	onSelect={() =>
+																		setTime(
+																			item.start_time ??
+																				item.sp_start_time
+																		)
+																	}>
+																	{item.start_time ??
+																		item.sp_start_time}
+																</DropdownMenu.Item>
+															)
+														)}
+													</DropdownMenu.Content>
+												</DropdownMenu.Portal>
+											</DropdownMenu.Root>
+										</div>
 										<input type="hidden" name="dropdown" value={time} />
 									</div>
 								</div>
 
 								<div className="flex justify-center md:justify-end mt-5">
 									<Button
+										disabled={time && selectDate ? false : true}
 										className="bg-primary rounded mx-1 hover:bg-primary"
 										type="submit"
 										name="Submit"
