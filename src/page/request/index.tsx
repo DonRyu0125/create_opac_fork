13efa6aca9b9ce9c6856ccf248d74923d263f4b1
donRyu0125 <don@minisisinc.com>
2025-07-23 16:32:31 -0700

    hotfix

--- src/page/request/index.tsx
+++ src/page/request/index.tsx
@@ -30,21 +30,22 @@ const Request = () => {
 							</h1>
 							<div className="text-right">
 								<form method="post" className="m-0" action={removeQuote(reqData.action)}>
-									{/* <Input type="hidden" name="auto_approve" value={reqData.auto_approve} />
-									<Input type="hidden" name="req_process_date" value={reqData.req_process_date} /> */}
 									<Input type="hidden" name="req_db_recid" value={reqData.req_db_recid} />
+									{/* 
+									<Input type="hidden" name="auto_approve" value={reqData.auto_approve} />
+									<Input type="hidden" name="req_process_date" value={reqData.req_process_date} />
 									<Input type="hidden" name="library_req" value={reqData.library_req} />
 									<Input type="hidden" name="req_pickup_loc" value={reqData.req_pickup_loc} />
-									{/* <Input type="hidden" name="method_request" value={reqData.method_request} />
+									<Input type="hidden" name="method_request" value={reqData.method_request} />
 									<Input type="hidden" name="req_topic" value={reqData.req_topic} />
-									<Input type="hidden" name="req_loc_code" value={reqData.req_loc_code} /> */}
-									{/* <Input type="hidden" name="req_appl_name" value={reqData.req_appl_name} /> */}
+									<Input type="hidden" name="req_loc_code" value={reqData.req_loc_code} />
+									<Input type="hidden" name="req_appl_name" value={reqData.req_appl_name} /> */}
 									{reqData.req_next_collect && <Input type="hidden" name="req_next_collect" value={reqData.req_next_collect} />}
 									{reqData.req_item_id && <Input type="hidden" name="req_item_id" value={reqData.req_item_id} />}
 									{reqData.req_db_link1 && <Input type="hidden" name="REQ_DB_LINK1" value={reqData.req_db_link1} />}
 									{reqData.req_db_link3 && <Input type="hidden" name="REQ_DB_LINK3" value={reqData.req_db_link3} />}
 									<Input type="hidden" name="req_status" value={reqData.req_status} />
-									<Input type="hidden" name="rec_status" value={reqData.rec_status ?? 'Active'} />
+									<Input type="hidden" name="rec_status" value={reqData.rec_status ?? 'ACTIVE'} />
 									<Input type="hidden" name="req_db_name" value={reqData.req_db_name} />
 									<Input type="hidden" name="date_needed" value={reqData.date_needed} />
 									<Input type="hidden" name="time_needed" value={reqData.time_needed} />
