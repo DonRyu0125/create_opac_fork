9b2829bdb0a996c136ab079e5a8d786df9d2d7f4
donRyu0125 <don@minisisinc.com>
2025-08-13 16:21:14 -0700

    PATRON_BIBLIO ERROR FIX

--- src/components/layouts/accountsettings/Header.tsx
+++ src/components/layouts/accountsettings/Header.tsx
@@ -61,7 +61,7 @@ const Header = () => {
 								<Link
 									href={
 										getCookieValue('HOME_SESSID') +
-										'?SEARCH&DATABASE=CLIENT&REPORT=WEB_LIBRARY_CIRC_DASHBOARD&EXP=patron_id+~3D+global(m2l_patron_id)'
+										'?SEARCH&DATABASE=PATRON_BIBLIO&REPORT=WEB_LIBRARY_CIRC_DASHBOARD&EXP=patron_id+~3D+global(m2l_patron_id)'
 									}
 									className="no-underline ml-[0.5rem]">
 									{message.patronDashboard}
