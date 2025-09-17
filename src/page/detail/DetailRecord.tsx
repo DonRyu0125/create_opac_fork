0fe4ed456aaed6709bb913cd654e7689d83a2cdd
donRyu0125 <don@minisisinc.com>
2025-09-17 10:15:11 -0700

    detailrecord duplication fix

--- src/page/detail/DetailRecord.tsx
+++ src/page/detail/DetailRecord.tsx
@@ -39,12 +39,18 @@ const DetailRecord = (props: Props) => {
 							)
 						}
 						if (Array.isArray(value)) {
-							return value.map((e, i) => (
-								<div key={i}>
-									<HighlightText text={e.toString()} highlights={searchTerms} />
-								</div>
-							))
+							return <div >
+								<HighlightText text={value[0].toString()} highlights={searchTerms} />
+							</div>
 						}
+						// if (Array.isArray(value)) {
+						// 	console.log('value',value)
+						// 	return value.map((e, i) => (
+						// 		<div key={i}>
+						// 			<HighlightText text={e.toString()} highlights={searchTerms} />
+						// 		</div>
+						// 	))
+						// }
 						return value
 					}}
 				/>
