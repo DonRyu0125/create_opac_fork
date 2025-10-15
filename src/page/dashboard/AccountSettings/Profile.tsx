388e8b75874715c8f66c97a6d7852010e91cbfcc
donRyu0125 <don@minisisinc.com>
2025-10-15 15:10:18 -0700

    add validation for profile change

--- src/page/dashboard/AccountSettings/Profile.tsx
+++ src/page/dashboard/AccountSettings/Profile.tsx
@@ -99,10 +99,10 @@ const Profile = () => {
 				<label htmlFor="name" className="text-sm font-medium">
 					Full Name
 				</label>
-				<div className="flex items-center">
+				<div className="flex items-center w-full">
 					<User className="mr-2 h-4 w-4 text-gray-500" />
-					<Input id="firstName" name="firstName" placeholder="Enter your first name" value={profileForm.firstName} onChange={handleProfileChange} />
-					<Input id="lastName" name="lastName" placeholder="Enter your last name" value={profileForm.lastName} onChange={handleProfileChange} />
+					<Input className='w-[40%] mr-10' id="firstName" name="firstName" placeholder="Enter your first name" value={profileForm.firstName} onChange={handleProfileChange} />
+					<Input className='w-[40%]' id="lastName" name="lastName" placeholder="Enter your last name" value={profileForm.lastName} onChange={handleProfileChange} />
 				</div>
 				{profileErrors.name && <p className="text-sm text-red-500">{profileErrors.name}</p>}
 			</div>
