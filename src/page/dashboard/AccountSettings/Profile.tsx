16e307ead7b12211e63c2cf44ec57ab68a655cd7
donRyu0125 <don@minisisinc.com>
2025-10-20 15:57:59 -0700

    add reset email confirm email

--- src/page/dashboard/AccountSettings/Profile.tsx
+++ src/page/dashboard/AccountSettings/Profile.tsx
@@ -1,10 +1,10 @@
 import { useState, type ChangeEvent, type FormEvent, useEffect } from 'react'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
-import { Bell, Key, LoaderCircle, Lock, Mail, Save, Shield, User, UserCog } from 'lucide-react'
+import { Bell, CheckCircle, Key, LoaderCircle, Lock, Mail, Save, Shield, User, UserCog } from 'lucide-react'
 import useJSONData from '@/hooks/useJSONData'
 import axios from 'axios'
-import { getCookieValue } from '@/lib/utils'
+import { encodeObj, getCookieValue, getSessionID } from '@/lib/utils'
 import { toast } from '@/components/ui/use-toast'
 
 // Define interfaces for form data and errors
@@ -21,7 +21,35 @@ interface ProfileFormErrors {
 
 const Profile = () => {
 	const { records } = useJSONData({ selector: '#xml_record' })
+	const [countdown, setCountdown] = useState(10)
 	const [loading, setLoadting] = useState(false)
+	const [profileForm, setProfileForm] = useState<ProfileFormData>({
+		firstName: records[0].first_name,
+		lastName: records[0].last_name,
+		email: records[0].email,
+	})
+	const [profileErrors, setProfileErrors] = useState<ProfileFormErrors>({
+		name: '',
+		email: '',
+	})
+	const [changedEmail, setChangedEmail] = useState(false)
+
+	useEffect(() => {
+		if (!changedEmail) return
+		const timer = setInterval(() => {
+			setCountdown((prev) => prev - 1)
+		}, 1000)
+
+		const redirect = setTimeout(() => {
+			window.location.href = '/'
+		}, 4000)
+
+		return () => {
+			clearInterval(timer)
+			clearTimeout(redirect)
+		}
+	}, [changedEmail])
+
 	const handleProfileChange = (e: ChangeEvent<HTMLInputElement>): void => {
 		const { name, value } = e.target
 		setProfileForm({
@@ -36,16 +64,6 @@ const Profile = () => {
 			})
 		}
 	}
-	// Profile form state
-	const [profileForm, setProfileForm] = useState<ProfileFormData>({
-		firstName: records[0].first_name,
-		lastName: records[0].last_name,
-		email: records[0].email,
-	})
-	const [profileErrors, setProfileErrors] = useState<ProfileFormErrors>({
-		name: '',
-		email: '',
-	})
 
 	// Validate profile form
 	const validateProfileForm = (): boolean => {
@@ -77,7 +95,6 @@ const Profile = () => {
 			headers: { 'Content-Type': 'text/xml' },
 			data: `<?xml version="1.0" encoding="UTF-8"?><RECORD><P_FIRST_NAME>${profileForm.firstName}</P_FIRST_NAME>
 			<P_LAST_NAME>${profileForm.lastName}</P_LAST_NAME>
-			<C_EMAIL>${profileForm.email}</C_EMAIL>
 			</RECORD>`,
 		})
 			.then(() => {
@@ -92,60 +109,146 @@ const Profile = () => {
 				})
 			})
 	}
+
+	const submitEmailChange = async () => {
+		let HOME_SESSID = getSessionID()
+		let is_french = getCookieValue('my_lang') === '145' ? true : false
+		const encoded = encodeObj(JSON.stringify({
+			client_number: records[0].client_number,
+			email: profileForm.email,
+		}))
+		setChangedEmail(true)
+		return await axios
+			.post(
+				`${HOME_SESSID}?SAVE_MAIL_FORM&TEMPLATE=[OPAC_EMAIL_TMP]${is_french ? 'EmailChgConfrim_fr.txt' : 'EmailChgConfirm.txt'}&FROM_DEFAULT=noreply@minisisinc.com&TO_DEFAULT=${profileForm.email}&SUBJECT_DEFAULT=${'Email Change Confirmation'}`,
+				{
+					client_number:  records[0].client_number,
+					EMAIL_CHG_LANDING_PAGE_URL:`${window.location.protocol}//${window.location.hostname}/reset-email.html`,
+					email: profileForm.email,
+					encoded,
+				},
+				{
+					headers: {
+						'Content-Type': 'multipart/form-data',
+					},
+				}
+			)
+			.then(() => {
+				setChangedEmail(false)
+			})
+	}
+
 	return (
-		<form onSubmit={handleProfileSubmit} className="space-y-6">
-			<div className="space-y-2">
-				<label htmlFor="name" className="text-sm font-medium">
-					Full Name
-				</label>
-				<div className="flex items-center w-full justify-between w-[70%]">
-					<div className="flex items-center w-[40%]">
-						<User className="mr-2 h-4 w-4 text-gray-500" />
-						<Input
-							id="firstName"
-							name="firstName"
-							placeholder="Enter your first name"
-							value={profileForm.firstName}
-							onChange={handleProfileChange}
-						/>
+		<>
+			{changedEmail ? (
+				<section className="flex items-center justify-center space-y-6 min-h-[364px]">
+					<div className="text-center space-y-4">
+						<h1 className="flex justify-center items-center text-2xl font-bold text-black">
+							<span className="mr-3">
+								<CheckCircle className="mr-3 h-6 w-6 text-black" />
+							</span>
+							We sent a confirmation email
+						</h1>
+						<p className="mt-6 text-lg text-black">
+							Please check your email to finish changing your email address.
+							<a className="font-semibold" href="/">
+								log in again
+							</a>
+							.
+						</p>
+						<p className="mt-4 text-black">
+							Redirecting in {countdown} second{countdown !== 1 ? 's' : ''}...
+						</p>
 					</div>
-					<Input
-						className="w-[50%]"
-						id="lastName"
-						name="lastName"
-						placeholder="Enter your last name"
-						value={profileForm.lastName}
-						onChange={handleProfileChange}
-					/>
-				</div>
-				{profileErrors.name && <p className="text-sm text-red-500">{profileErrors.name}</p>}
-			</div>
+				</section>
+			) : (
+				<>
+					<form onSubmit={handleProfileSubmit} className="space-y-6">
+						<div className="space-y-2">
+							<label htmlFor="name" className="text-sm font-medium">
+								Full Name
+							</label>
+							<div className="flex items-center w-full justify-between w-[70%]">
+								<div className="flex items-center w-[40%]">
+									<User className="mr-2 h-4 w-4 text-gray-500" />
+									<Input
+										id="firstName"
+										name="firstName"
+										placeholder="Enter your first name"
+										value={profileForm.firstName}
+										onChange={handleProfileChange}
+									/>
+								</div>
+								<Input
+									className="w-[50%]"
+									id="lastName"
+									name="lastName"
+									placeholder="Enter your last name"
+									value={profileForm.lastName}
+									onChange={handleProfileChange}
+								/>
+							</div>
+							{profileErrors.name && <p className="text-sm text-red-500">{profileErrors.name}</p>}
+						</div>
 
-			<div className="space-y-2">
-				<label htmlFor="email" className="text-sm font-medium">
-					Email Address
-				</label>
-				<div className="flex items-center">
-					<Mail className="mr-2 h-4 w-4 text-gray-500" />
-					<Input id="email" name="email" placeholder="Enter your email" value={profileForm.email} onChange={handleProfileChange} />
-				</div>
-				<p className="text-sm text-gray-500">This email will be used for account notifications.</p>
-				{profileErrors.email && <p className="text-sm text-red-500">{profileErrors.email}</p>}
-			</div>
+						<div className="space-y-2">
+							<label htmlFor="email" className="text-sm font-medium">
+								Email Address
+							</label>
+							<div className="flex items-center">
+								<Mail className="mr-2 h-4 w-4 text-gray-500" />
+								<Input
+									id="email"
+									name="email"
+									placeholder="Enter your email"
+									value={profileForm.email}
+									onChange={handleProfileChange}
+								/>
+							</div>
+							<p className="text-sm text-gray-500">This email will be used for account notifications.</p>
+							{profileErrors.email && <p className="text-sm text-red-500">{profileErrors.email}</p>}
+						</div>
 
-			<Button type="submit" className="bg-black hover:bg-gray-800">
-				{loading ? (
-					<>
-						<LoaderCircle />
-					</>
-				) : (
-					<>
-						<Save className="mr-2 h-4 w-4" />
-						Save Profile
-					</>
-				)}
-			</Button>
-		</form>
+						<Button type="submit" className="bg-black hover:bg-gray-800">
+							{loading ? (
+								<>
+									<LoaderCircle />
+								</>
+							) : (
+								<>
+									<Save className="mr-2 h-4 w-4" />
+									Save Profile
+								</>
+							)}
+						</Button>
+					</form>
+					<div className="space-y-6">
+						<div className="space-y-2">
+							<label htmlFor="email" className="text-sm font-medium">
+								Email Address
+							</label>
+							<div className="flex items-center">
+								<Mail className="mr-2 h-4 w-4 text-gray-500" />
+								<Input
+									id="email"
+									name="email"
+									placeholder="Enter your email"
+									value={profileForm.email}
+									onChange={handleProfileChange}
+								/>
+							</div>
+							<p className="text-sm text-gray-500">This email will be used for account notifications.</p>
+							{profileErrors.email && <p className="text-sm text-red-500">{profileErrors.email}</p>}
+						</div>
+
+						<Button className="bg-black hover:bg-gray-800" onClick={submitEmailChange}>
+							<Save className="mr-2 h-4 w-4" />
+							Change Email
+						</Button>
+					</div>
+				</>
+			)}
+		</>
 	)
 }
 
